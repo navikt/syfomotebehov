ae38f3d1fb082150b3fa8bcabb9b43e60720ef37
June Henriksen <june.henriksen2@nav.no>
2018-07-03 17:09:07 +0200

    SYFOUTV-2191: Tillat kun boolske verdier i boolean "hack" for oracle
    Oracle har ikke en datatype for boolean. Løsningen her er en boolean som
    vanlig i Java, som blir lagrer ned i Oracle som et ensifret nummer, med en
    sjekk som kun tillater 0 og 1.

--- src/main/resources/db/migration/V1.0_2_1__dialogmotebehov_table.sql
+++ src/main/resources/db/migration/V1.0_2_1__dialogmotebehov_table.sql
@@ -5,6 +5,6 @@ CREATE TABLE DIALOGMOTEBEHOV (
   tidspunkt_friskmelding     VARCHAR(1200),
   tiltak                     VARCHAR(1200),
   resultat_tiltak            VARCHAR(1200),
-  trenger_mote               NUMBER(1,0),
+  trenger_mote               NUMBER CHECK(trenger_mote IN (1,0)),
   behov_dialogmote           VARCHAR(1200)
 );
