5bb5371288b45593f7fc07f6ac403eba2c6a9b4e
Erik Jansen <erik.gunnar.jansen@nav.no>
2018-07-18 11:09:22 +0200

    Gjoer isAlive og isReady unprotected

--- src/main/java/no/nav/syfo/web/selftest/SelftestController.java
+++ src/main/java/no/nav/syfo/web/selftest/SelftestController.java
@@ -1,6 +1,7 @@
 package no.nav.syfo.web.selftest;
 
 import lombok.extern.slf4j.Slf4j;
+import no.nav.security.spring.oidc.validation.api.Unprotected;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,17 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@Unprotected
 @RequestMapping(value = "/internal")
 public class SelftestController {
     private static final String APPLICATION_LIVENESS = "Application is alive!";
     private static final String APPLICATION_READY = "Application is ready!";
 
+    @Unprotected
     @ResponseBody
     @RequestMapping(value = "/isAlive", produces = MediaType.TEXT_PLAIN_VALUE)
     public String isAlive() {
         return APPLICATION_LIVENESS;
     }
 
+    @Unprotected
     @ResponseBody
     @RequestMapping(value = "/isReady", produces = MediaType.TEXT_PLAIN_VALUE)
     public String isReady() {
