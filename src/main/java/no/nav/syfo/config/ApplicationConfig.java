a541208b711427cf5644f6792db818cba0a4b3d5
Erik Jansen <erik.gunnar.jansen@nav.no>
2018-07-18 10:29:01 +0200

    Legg til transactionManagement og oppdater application.yaml

--- src/main/java/no/nav/syfo/config/ApplicationConfig.java
+++ src/main/java/no/nav/syfo/config/ApplicationConfig.java
@@ -1,9 +1,11 @@
 package no.nav.syfo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @Configuration
+@EnableTransactionManagement
 public class ApplicationConfig {
 }
 
