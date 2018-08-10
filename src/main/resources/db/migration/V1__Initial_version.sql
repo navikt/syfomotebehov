2b4a67e7433a1edacbc4bd4944614f536aa39c91
June Henriksen <june.henriksen2@nav.no>
2018-08-10 12:47:28 +0200

    SYFOUTV-2191: Endre migreringsscript for å støtte postgres

--- src/main/resources/db/migration/V1__Initial_version.sql
+++ src/main/resources/db/migration/V1__Initial_version.sql
@@ -6,14 +6,13 @@
 -- ROLLBACK-END
 
 CREATE TABLE MOTEBEHOV (
-  motebehov_uuid        VARCHAR(36) NOT NULL,
+  motebehov_uuid              VARCHAR(36) NOT NULL PRIMARY KEY,
   opprettet_dato              TIMESTAMP NOT NULL,
   opprettet_av                VARCHAR(13) NOT NULL,
   aktoer_id                   VARCHAR(13) NOT NULL,
-  friskmelding_forventning    CLOB,
-  tiltak                      CLOB,
-  tiltak_resultat             CLOB,
-  har_motebehov               NUMBER CHECK (har_motebehov IN (1, 0)),
-  forklaring                  CLOB,
-  CONSTRAINT MOTEBEHOV_PK PRIMARY KEY (motebehov_uuid)
+  friskmelding_forventning    VARCHAR(MAX),
+  tiltak                      VARCHAR(MAX),
+  tiltak_resultat             VARCHAR(MAX),
+  har_motebehov               BOOLEAN,
+  forklaring                  VARCHAR(MAX)
 );
