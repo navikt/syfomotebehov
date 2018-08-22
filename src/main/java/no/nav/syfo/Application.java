a9e9b76444d4bbc6019717e586c9853643ce7b07
Erik Jansen <erik.gunnar.jansen@nav.no>
2018-08-22 16:22:56 +0200

    Legg til syfomotebehov-oidc

--- src/main/java/no/nav/syfo/Application.java
+++ src/main/java/no/nav/syfo/Application.java
@@ -4,6 +4,9 @@ import no.nav.security.spring.oidc.validation.api.EnableOIDCTokenValidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static java.lang.System.getProperty;
+import static java.lang.System.setProperty;
+
 @SpringBootApplication
 @EnableOIDCTokenValidation(ignore="org.springframework")
 public class Application {
