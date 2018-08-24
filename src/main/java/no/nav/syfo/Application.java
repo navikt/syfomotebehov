e66c70e315c59343350ecda6a9aeef16a1a0bed9
Erik Jansen <erik.gunnar.jansen@nav.no>
2018-08-24 10:57:50 +0200

    Fjern oidc validation

--- src/main/java/no/nav/syfo/Application.java
+++ src/main/java/no/nav/syfo/Application.java
@@ -1,14 +1,9 @@
 package no.nav.syfo;
 
-import no.nav.security.spring.oidc.validation.api.EnableOIDCTokenValidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static java.lang.System.getProperty;
-import static java.lang.System.setProperty;
-
 @SpringBootApplication
-@EnableOIDCTokenValidation(ignore="org.springframework")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
