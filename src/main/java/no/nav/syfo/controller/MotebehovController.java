0001589410eeb2f74b31179d04fc32aa7ef94a83
Tor-Christian Tangenes <Tor-Christian.Tangenes@nav.no>
2018-08-31 10:24:11 +0200

    Legg eget endepunkt for veileder med @CrossOrigin: /api/veileder/motebehov

--- src/main/java/no/nav/syfo/controller/MotebehovController.java
+++ src/main/java/no/nav/syfo/controller/MotebehovController.java
@@ -36,7 +36,7 @@ public class MotebehovController {
 
     @Inject
     public MotebehovController(final OIDCRequestContextHolder contextHolder,
-                               final MotebehovService motebehovService){
+                               final MotebehovService motebehovService) {
         this.contextHolder = contextHolder;
         this.motebehovService = motebehovService;
     }
@@ -46,7 +46,7 @@ public class MotebehovController {
     public List<Motebehov> hentMotebehovListe(
             @RequestParam(name = "fnr") @Pattern(regexp = "^[0-9]{11}$") String arbeidstakerFnr,
             @RequestParam(name = "virksomhetsnummer") String virksomhetsnummer
-        ) {
+    ) {
         if (Toggle.endepunkterForMotebehov) {
             if (!virksomhetsnummer.isEmpty()) {
                 return motebehovService.hentMotebehovListe(Fnr.of(arbeidstakerFnr), virksomhetsnummer);
