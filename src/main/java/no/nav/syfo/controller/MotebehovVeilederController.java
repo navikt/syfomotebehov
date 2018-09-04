548f9b20a826f9959a2a644e8d562b56aebb2ce6
Erik Jansen <erik.gunnar.jansen@nav.no>
2018-09-04 08:29:02 +0200

    rydd opp

--- src/main/java/no/nav/syfo/controller/MotebehovVeilederController.java
+++ src/main/java/no/nav/syfo/controller/MotebehovVeilederController.java
@@ -1,14 +1,9 @@
 package no.nav.syfo.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import no.nav.security.oidc.OIDCConstants;
-import no.nav.security.oidc.context.OIDCRequestContextHolder;
-import no.nav.security.oidc.context.OIDCValidationContext;
-import no.nav.security.spring.oidc.validation.api.ProtectedWithClaims;
 import no.nav.security.spring.oidc.validation.api.Unprotected;
 import no.nav.syfo.domain.rest.Fnr;
 import no.nav.syfo.domain.rest.Motebehov;
-import no.nav.syfo.domain.rest.NyttMotebehov;
 import no.nav.syfo.service.MotebehovService;
 import no.nav.syfo.util.Toggle;
 import org.springframework.web.bind.annotation.*;
@@ -16,11 +11,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolationException;
-import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
 import java.io.IOException;
 import java.util.List;
-import java.util.UUID;
 
 import static java.util.Collections.emptyList;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
@@ -32,18 +25,15 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequestMapping(value = "/api/veileder/motebehov")
 public class MotebehovVeilederController {
 
-    private OIDCRequestContextHolder contextHolder;
     private MotebehovService motebehovService;
 
     @Inject
-    public MotebehovVeilederController(final OIDCRequestContextHolder contextHolder,
-                                       final MotebehovService motebehovService){
-        this.contextHolder = contextHolder;
+    public MotebehovVeilederController(final MotebehovService motebehovService) {
         this.motebehovService = motebehovService;
     }
 
     @ResponseBody
-    @CrossOrigin(origins = "https://app-q1.adeo.no", allowCredentials = "true")
+    @CrossOrigin(allowCredentials = "true")
     @GetMapping(produces = APPLICATION_JSON_VALUE)
     public List<Motebehov> hentMotebehovListe(
             @RequestParam(name = "fnr") @Pattern(regexp = "^[0-9]{11}$") String arbeidstakerFnr) {
