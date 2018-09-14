0fbe05668883f581d0c2c4ef6fdb3ab7e14515a6
Tor-Christian Tangenes <Tor-Christian.Tangenes@nav.no>
2018-09-14 13:34:55 +0200

    Debug

--- src/main/java/no/nav/syfo/util/AuthorizationFilterUtils.java
+++ src/main/java/no/nav/syfo/util/AuthorizationFilterUtils.java
@@ -2,18 +2,23 @@ package no.nav.syfo.util;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Base64;
+import lombok.extern.slf4j.Slf4j;
 
 import static java.lang.String.format;
 import static java.lang.System.getProperty;
 import static java.util.Optional.ofNullable;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
+@Slf4j
 public class AuthorizationFilterUtils {
     public static boolean erRequestAutorisert(HttpServletRequest httpServletRequest, String credential) {
         return ofNullable(httpServletRequest.getHeader(AUTHORIZATION)).map(credential::equals).orElse(false);
     }
 
     public static String basicCredentials(String credential) {
+        log.info("Cred: " + credential);
+        log.info("User: " + getProperty(credential + ".username"));
+        log.info("Pass: " + getProperty(credential + ".password"));
         return "Basic " + Base64.getEncoder().encodeToString(format("%s:%s", getProperty(credential + ".username"), getProperty(credential + ".password")).getBytes());
     }
 }
