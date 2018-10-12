786a5b35f095adb0e18cb396f905586fede5bf67
Erik Jansen <erik.gunnar.jansen@nav.no>
2018-10-12 15:59:20 +0200

    TC

--- src/main/java/no/nav/syfo/config/ws/SykefravaersoppfoelgingConfig.java
+++ src/main/java/no/nav/syfo/config/ws/SykefravaersoppfoelgingConfig.java
@@ -11,16 +11,11 @@ import no.nav.tjeneste.virksomhet.sykefravaersoppfoelging.v1.meldinger.WSHentNae
 import no.nav.tjeneste.virksomhet.sykefravaersoppfoelging.v1.meldinger.WSHentNaermesteLedersAnsattListeResponse;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.frontend.ClientProxy;
-import org.apache.cxf.message.Message;
-import org.apache.cxf.phase.PhaseInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-
-import javax.xml.ws.handler.Handler;
-import java.util.List;
 
 import static java.util.Collections.singletonList;
 
@@ -31,14 +26,16 @@ public class SykefravaersoppfoelgingConfig {
     @Value("${sykefravaersoppfoelging.v1.endpointurl}")
     protected String serviceUrl;
 
-    private SykefravaersoppfoelgingV1 proxy;
+    private SykefravaersoppfoelgingV1 port;
 
     @SuppressWarnings("unchecked")
     @Bean
     @ConditionalOnProperty(value = "mockSykefravaeroppfoelging_V1", havingValue = "false", matchIfMissing = true)
     @Primary
     public SykefravaersoppfoelgingV1 sykefravaersoppfoelgingV1() {
-        SykefravaersoppfoelgingV1 port = createPort(SykefravaersoppfoelgingV1.class, singletonList(new LogErrorHandler()));
+        SykefravaersoppfoelgingV1 port = new WsClient<SykefravaersoppfoelgingV1>().createPort(serviceUrl, SykefravaersoppfoelgingV1.class, singletonList(new LogErrorHandler()));
+        STSClientConfig.configureRequestSamlTokenOnBehalfOfOidc(port);
+        this.port = port;
         return port;
     }
 
@@ -46,31 +43,18 @@ public class SykefravaersoppfoelgingConfig {
     public SykefravaersoppfoelgingConfig() {
     }
 
-    public SykefravaersoppfoelgingV1 proxy() {
-        if (proxy == null) {
-            this.proxy = createPort(SykefravaersoppfoelgingV1.class, singletonList(new LogErrorHandler()));
-        }
-        return this.proxy;
-    }
+    public WSHentNaermesteLedersAnsattListeResponse hentNaermesteLedersAnsattListe(WSHentNaermesteLedersAnsattListeRequest request, String OIDCToken) throws HentNaermesteLedersAnsattListeSikkerhetsbegrensning {
+        leggTilOidcOnBehalfOfOutInterceptor(port, OIDCToken);
 
-    public SykefravaersoppfoelgingV1 createPort(Class<?> portType, List<Handler> handlers, PhaseInterceptor<? extends Message>... interceptors) {
-        SykefravaersoppfoelgingV1 port = new WsClient<SykefravaersoppfoelgingV1>().createPort(serviceUrl, portType, handlers, interceptors);
-
-        STSClientConfig.configureRequestSamlTokenOnBehalfOfOidc(port);
-        return port;
-    }
-
-    public WSHentNaermesteLedersAnsattListeResponse hentNaermesteLedersAnsattListe(WSHentNaermesteLedersAnsattListeRequest request, String oidcToken) throws HentNaermesteLedersAnsattListeSikkerhetsbegrensning {
-
-        Client c = ClientProxy.getClient(proxy());
-        c.getRequestContext().put(OnBehalfOfOutInterceptor.REQUEST_CONTEXT_ONBEHALFOF_TOKEN_TYPE, OnBehalfOfOutInterceptor.TokenType.OIDC);
-        c.getRequestContext().put(OnBehalfOfOutInterceptor.REQUEST_CONTEXT_ONBEHALFOF_TOKEN, oidcToken);
-
-        log.info("JTRACE oidcToken {}", oidcToken);
+        log.info("JTRACE oidcToken {}", OIDCToken);
         log.info("JTRACE aktoerId {}", request.getAktoerId());
 
-        return proxy.hentNaermesteLedersAnsattListe(request);
+        return port.hentNaermesteLedersAnsattListe(request);
     }
 
-
+    private void leggTilOidcOnBehalfOfOutInterceptor(SykefravaersoppfoelgingV1 port, String OIDCToken) {
+        Client c = ClientProxy.getClient(port);
+        c.getRequestContext().put(OnBehalfOfOutInterceptor.REQUEST_CONTEXT_ONBEHALFOF_TOKEN_TYPE, OnBehalfOfOutInterceptor.TokenType.OIDC);
+        c.getRequestContext().put(OnBehalfOfOutInterceptor.REQUEST_CONTEXT_ONBEHALFOF_TOKEN, OIDCToken);
+    }
 }
