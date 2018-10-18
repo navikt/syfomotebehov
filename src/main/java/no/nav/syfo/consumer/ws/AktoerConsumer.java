9262ae83fdca42ae3586283f705413127414aa63
Erik Jansen <erik.gunnar.jansen@nav.no>
2018-10-18 08:34:48 +0200

    Basic cache

--- src/main/java/no/nav/syfo/consumer/ws/AktoerConsumer.java
+++ src/main/java/no/nav/syfo/consumer/ws/AktoerConsumer.java
@@ -7,6 +7,7 @@ import no.nav.tjeneste.virksomhet.aktoer.v2.HentIdentForAktoerIdPersonIkkeFunnet
 import no.nav.tjeneste.virksomhet.aktoer.v2.meldinger.WSHentAktoerIdForIdentRequest;
 import no.nav.tjeneste.virksomhet.aktoer.v2.meldinger.WSHentIdentForAktoerIdRequest;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
@@ -33,6 +34,7 @@ public class AktoerConsumer implements InitializingBean {
         this.aktoerV2 = aktoerV2;
     }
 
+    @Cacheable
     public String hentAktoerIdForFnr(String fnr) {
         try {
             return aktoerV2.hentAktoerIdForIdent(new WSHentAktoerIdForIdentRequest()
