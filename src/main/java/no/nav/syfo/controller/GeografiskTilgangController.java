9c1816af814e443290786519f606f3a51284c7ab
John Martin Lindseth <john.martin.lindseth@nav.no>
2018-10-22 14:06:44 +0200

    Legg til endepunkt for geografisk tilgang
    SYFOUTV-2556

---
+++ src/main/java/no/nav/syfo/controller/GeografiskTilgangController.java
@@ -0,0 +1,44 @@
+package no.nav.syfo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import no.nav.security.spring.oidc.validation.api.ProtectedWithClaims;
+import no.nav.syfo.service.GeografiskTilgangService;
+import no.nav.syfo.util.Toggle;
+import org.springframework.web.bind.annotation.*;
+
+import javax.inject.Inject;
+import javax.validation.constraints.Pattern;
+import javax.ws.rs.core.Response;
+
+import static no.nav.syfo.OIDCIssuer.INTERN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/api/geografisktilgang")
+public class GeografiskTilgangController {
+
+    private GeografiskTilgangService geografiskTilgangService;
+
+    @Inject
+    public GeografiskTilgangController(
+            final GeografiskTilgangService geografiskTilgangService) {
+        this.geografiskTilgangService = geografiskTilgangService;
+    }
+
+    @ResponseBody
+    @ProtectedWithClaims(issuer = INTERN, claimMap = {"sub=srvsyfoservice"})
+    @GetMapping(produces = APPLICATION_JSON_VALUE)
+    public Response hentMotebehovListe(@RequestParam(name = "fnr") @Pattern(regexp = "^[0-9]{11}$") String arbeidstakerFnr) {
+        if (Toggle.endepunkterForMotebehov) {
+            if (geografiskTilgangService.erBrukerTilhorendeMotebehovPilot(arbeidstakerFnr)) {
+                return Response.ok().build();
+            } else {
+                return Response.status(Response.Status.FORBIDDEN).build();
+            }
+        } else {
+            log.info("Det ble gjort kall mot 'geografisktilgang', men dette endepunktet er togglet av.");
+            return Response.status(Response.Status.FORBIDDEN).build();
+        }
+    }
+}
