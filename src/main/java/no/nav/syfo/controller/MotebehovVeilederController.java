0e7f6dfb19905a8ddc85154007f437acb0673d7b
Erik Jansen <erik.gunnar.jansen@nav.no>
2018-11-01 10:49:45 +0100

    TC

--- src/main/java/no/nav/syfo/controller/MotebehovVeilederController.java
+++ src/main/java/no/nav/syfo/controller/MotebehovVeilederController.java
@@ -55,6 +55,7 @@ public class MotebehovVeilederController {
     @RequestMapping(value = "/motebehov")
     @ProtectedWithClaims(issuer = INTERN)
     @GetMapping(produces = APPLICATION_JSON_VALUE)
+    @CrossOrigin(allowCredentials = "true", origins = "https://modiasyfofront-q1.nais.preprod.local")
     public List<Motebehov> hentMotebehovListe(@RequestParam(name = "fnr") @Pattern(regexp = "^[0-9]{11}$") String arbeidstakerFnr) {
         if (Toggle.endepunkterForMotebehov) {
             kastExceptionHvisIkkeTilgang(arbeidstakerFnr);
@@ -70,6 +71,7 @@ public class MotebehovVeilederController {
     @RequestMapping(value = "/historikk")
     @ProtectedWithClaims(issuer = INTERN)
     @GetMapping(produces = APPLICATION_JSON_VALUE)
+    @CrossOrigin(allowCredentials = "true", origins = "https://modiasyfofront-q1.nais.preprod.local")
     public List<Historikk> hentMotebehovHistorikk(@RequestParam(name = "fnr") @Pattern(regexp = "^[0-9]{11}$") String arbeidstakerFnr) {
         if (Toggle.endepunkterForMotebehov) {
             kastExceptionHvisIkkeTilgang(arbeidstakerFnr);
