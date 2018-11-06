35fefcbd008b805835711f4588f5243973d3cecf
Erik Jansen <erik.gunnar.jansen@nav.no>
2018-11-06 15:29:23 +0100

    TC

--- src/main/java/no/nav/syfo/Application.java
+++ src/main/java/no/nav/syfo/Application.java
@@ -1,13 +1,13 @@
 package no.nav.syfo;
 
-import no.nav.security.spring.oidc.validation.api.EnableOIDCTokenValidation;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-@EnableOIDCTokenValidation(ignore="org.springframework")
+@EnableAutoConfiguration
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/no/nav/syfo/config/ApplicationConfig.java
+++ src/main/java/no/nav/syfo/config/ApplicationConfig.java
@@ -1,6 +1,7 @@
 package no.nav.syfo.config;
 
 import no.nav.security.oidc.context.OIDCRequestContextHolder;
+import no.nav.security.spring.oidc.validation.api.EnableOIDCTokenValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import static java.util.Arrays.asList;
 
 @Configuration
 @EnableTransactionManagement
+@EnableOIDCTokenValidation(ignore = "org.springframework")
 public class ApplicationConfig {
 
     @Bean
