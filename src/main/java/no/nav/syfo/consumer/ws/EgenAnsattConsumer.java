a1c5a7121f1b8ae2a2df3de498b7d2a1f25dcf92
Tor-Christian Tangenes <Tor-Christian.Tangenes@nav.no>
2018-11-13 09:34:08 +0100

    Maa hente overordnet enhet hvis sykmeldt er egen ansatt
    SYFOUTV-2532

---
+++ src/main/java/no/nav/syfo/consumer/ws/EgenAnsattConsumer.java
@@ -0,0 +1,38 @@
+package no.nav.syfo.consumer.ws;
+
+import lombok.extern.slf4j.Slf4j;
+import no.nav.tjeneste.pip.egen.ansatt.v1.EgenAnsattV1;
+import no.nav.tjeneste.pip.egen.ansatt.v1.WSHentErEgenAnsattEllerIFamilieMedEgenAnsattRequest;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+
+@Component
+@Slf4j
+public class EgenAnsattConsumer implements InitializingBean {
+    private static EgenAnsattConsumer instance;
+    private final EgenAnsattV1 egenAnsattV1;
+
+    @Override
+    public void afterPropertiesSet() { instance = this; }
+
+    public static EgenAnsattConsumer egenAnsattConsumer() { return instance; }
+
+    @Inject
+    public EgenAnsattConsumer(EgenAnsattV1 egenAnsattV1) {
+        this.egenAnsattV1 = egenAnsattV1;
+    }
+
+    public boolean erEgenAnsatt(String fnr) {
+        try {
+            return egenAnsattV1.hentErEgenAnsattEllerIFamilieMedEgenAnsatt(new WSHentErEgenAnsattEllerIFamilieMedEgenAnsattRequest()
+                    .withIdent(fnr)
+            ).isEgenAnsatt();
+        } catch (RuntimeException e) {
+            log.error("Klarte sjekke egenansatt status på sykmeldt");
+            throw e;
+        }
+    }
+
+}
