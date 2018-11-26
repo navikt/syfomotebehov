5d49d18011cbfaa5ec2c859e2bd189fcb0841d08
Erik Jansen <erik.gunnar.jansen@nav.no>
2018-11-26 15:30:29 +0100

    Del opp metrikker basert paa brukertype: AT/AG

--- src/main/java/no/nav/syfo/util/Metrikk.java
+++ src/main/java/no/nav/syfo/util/Metrikk.java
@@ -11,25 +11,34 @@ public class Metrikk {
 
     private final MeterRegistry registry;
 
+    public enum BRUKER {
+        ARBEIDSTAKER,
+        ARBEIDSGIVER,
+    }
+
     @Inject
     public Metrikk(MeterRegistry registry) {
         this.registry = registry;
     }
 
-    public void tellMotebehovBesvart(boolean harMotebehov) {
+    public void tellMotebehovBesvart(boolean harMotebehov, BRUKER bruker) {
         registry.counter(
                 "syfomotebehov_motebehov_besvart",
                 Tags.of(
                         "type", "info",
-                        "motebehov", harMotebehov ? "ja" : "nei"
+                        "motebehov", harMotebehov ? "ja" : "nei",
+                        "bruker", bruker.name().toLowerCase()
                 )
         ).increment();
     }
 
-    public void tellMotebehovBesvartNeiAntallTegn(int antallTegnIForklaring) {
+    public void tellMotebehovBesvartNeiAntallTegn(int antallTegnIForklaring, BRUKER bruker) {
         registry.counter(
                 "syfomotebehov_motebehov_besvart_nei_forklaring_lengde",
-                Tags.of("type", "info")
+                Tags.of(
+                        "type", "info",
+                        "bruker", bruker.name().toLowerCase()
+                )
         ).increment(antallTegnIForklaring);
     }
 }
