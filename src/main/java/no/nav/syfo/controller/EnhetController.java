bb51ee61622144ce6a172fa040e65710661a5018
Tor-Christian Tangenes <Tor-Christian.Tangenes@nav.no>
2018-11-30 13:29:04 +0100

    Legger til resttjeneste for aa hente sykmeldte med motebehov per enhet
    SYFOUTV-2536

---
+++ src/main/java/no/nav/syfo/controller/EnhetController.java
@@ -0,0 +1,41 @@
+package no.nav.syfo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import no.nav.security.spring.oidc.validation.api.ProtectedWithClaims;
+import no.nav.syfo.domain.rest.BrukerPaaEnhet;
+import no.nav.syfo.service.MotebehovService;
+import no.nav.syfo.service.VeilederTilgangService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Pattern;
+import javax.ws.rs.ForbiddenException;
+import java.util.List;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static no.nav.syfo.OIDCIssuer.INTERN;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/api/enheter")
+public class EnhetController {
+
+    private MotebehovService motebehovService;
+
+    private VeilederTilgangService veilederTilgangService;
+
+    public EnhetController(final MotebehovService motebehovService,
+                           final VeilederTilgangService veilederTilgangService) {
+        this.motebehovService = motebehovService;
+        this.veilederTilgangService = veilederTilgangService;
+    }
+
+    @ResponseBody
+    @ProtectedWithClaims(issuer = INTERN)
+    @GetMapping(value = "/{enhet}/sykmeldte", produces = APPLICATION_JSON)
+    public List<BrukerPaaEnhet> hentSykmeldteMedMotebehovSvarPaaEnhet
+            (@PathVariable @Pattern(regexp = "\\d{4}$") String enhet) {
+        if (!veilederTilgangService.sjekkVeiledersTilgangTilEnhet(enhet))
+            throw new ForbiddenException("Innlogget bruker har ikke tilgang til følgende enhet: " + enhet);
+        return motebehovService.hentSykmeldteMedMotebehovPaaEnhet(enhet);
+    }
+}
