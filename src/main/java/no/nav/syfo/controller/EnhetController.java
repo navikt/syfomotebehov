5b56be9d2aec7cd354f85908f9de424ee67441ac
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-03-07 14:33:32 +0100

    Legg til exceptionhandler i EnhetRessurs for aa mappe 403 til JSON

--- src/main/java/no/nav/syfo/controller/EnhetController.java
+++ src/main/java/no/nav/syfo/controller/EnhetController.java
@@ -7,12 +7,15 @@ import no.nav.syfo.service.MotebehovService;
 import no.nav.syfo.service.VeilederTilgangService;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.constraints.Pattern;
 import javax.ws.rs.ForbiddenException;
+import java.io.IOException;
 import java.util.List;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static no.nav.syfo.OIDCIssuer.AZURE;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
 
 @Slf4j
 @RestController
@@ -29,7 +32,6 @@ public class EnhetController {
         this.veilederTilgangService = veilederTilgangService;
     }
 
-    @ResponseBody
     @ProtectedWithClaims(issuer = AZURE)
     @GetMapping(value = "/{enhet}/motebehov/brukere", produces = APPLICATION_JSON)
     public List<BrukerPaaEnhet> hentSykmeldteMedMotebehovSvarPaaEnhet
@@ -37,5 +39,10 @@ public class EnhetController {
         if (!veilederTilgangService.sjekkVeiledersTilgangTilEnhet(enhet))
             throw new ForbiddenException("Innlogget bruker har ikke tilgang til følgende enhet: " + enhet);
         return motebehovService.hentSykmeldteMedMotebehovPaaEnhet(enhet);
+    }
+
+    @ExceptionHandler({ForbiddenException.class})
+    void handleForbiddenRequests(HttpServletResponse response) throws IOException {
+        response.sendError(FORBIDDEN.value(), "Handling er forbudt");
     }
 }
