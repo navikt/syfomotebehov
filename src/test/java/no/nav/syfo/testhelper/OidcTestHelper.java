dfca92f72c11ceae61043ccacef71df6bb2872f2
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-05-20 13:29:08 +0200

    Refaktorer OidcTestHelper

--- src/test/java/no/nav/syfo/testhelper/OidcTestHelper.java
+++ src/test/java/no/nav/syfo/testhelper/OidcTestHelper.java
@@ -1,29 +1,25 @@
 package no.nav.syfo.testhelper;
 
 import com.nimbusds.jwt.SignedJWT;
-import no.nav.security.oidc.context.OIDCClaims;
-import no.nav.security.oidc.context.OIDCRequestContextHolder;
-import no.nav.security.oidc.context.OIDCValidationContext;
-import no.nav.security.oidc.context.TokenContext;
+import no.nav.security.oidc.context.*;
 import no.nav.security.spring.oidc.test.JwtTokenGenerator;
+
+import static no.nav.syfo.OIDCIssuer.EKSTERN;
+import static no.nav.syfo.OIDCIssuer.INTERN;
 
 public class OidcTestHelper {
 
     public static void loggInnBruker(OIDCRequestContextHolder oidcRequestContextHolder, String subject) {
-        //OIDC-hack - legg til token og oidcclaims for en test-person
         SignedJWT jwt = JwtTokenGenerator.createSignedJWT(subject);
-        String issuer = "selvbetjening";
-        TokenContext tokenContext = new TokenContext(issuer, jwt.serialize());
-        OIDCClaims oidcClaims = new OIDCClaims(jwt);
-        OIDCValidationContext oidcValidationContext = new OIDCValidationContext();
-        oidcValidationContext.addValidatedToken(issuer, tokenContext, oidcClaims);
-        oidcRequestContextHolder.setOIDCValidationContext(oidcValidationContext);
+        settOIDCValidationContext(oidcRequestContextHolder, jwt, EKSTERN);
     }
 
     public static void loggInnVeileder(OIDCRequestContextHolder oidcRequestContextHolder, String subject) {
-        //OIDC-hack - legg til token og oidcclaims for en test-person
         SignedJWT jwt = JwtTokenGenerator.createSignedJWT(subject);
-        String issuer = "intern";
+        settOIDCValidationContext(oidcRequestContextHolder, jwt, INTERN);
+    }
+
+    private static void settOIDCValidationContext(OIDCRequestContextHolder oidcRequestContextHolder, SignedJWT jwt, String issuer) {
         TokenContext tokenContext = new TokenContext(issuer, jwt.serialize());
         OIDCClaims oidcClaims = new OIDCClaims(jwt);
         OIDCValidationContext oidcValidationContext = new OIDCValidationContext();
