f2b7a4405a18ec9b583cf3086b2a498904212eb4
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-06-03 09:09:51 +0200

    Fjern logspam fra atomikos
    Log: WARNING: transaction manager not running?

--- src/main/java/no/nav/syfo/config/ApplicationConfig.java
+++ src/main/java/no/nav/syfo/config/ApplicationConfig.java
@@ -1,17 +1,26 @@
 package no.nav.syfo.config;
 
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.jta.JtaTransactionManager;
 import org.springframework.web.client.RestTemplate;
 
 import static java.util.Arrays.asList;
 
-
 @Configuration
 @EnableTransactionManagement
 public class ApplicationConfig {
+
+    // Sørger for at flyway migrering skjer etter at JTA transaction manager er ferdig satt opp av Spring.
+    // Forhindrer WARNING: transaction manager not running? loggspam fra Atomikos.
+    @Bean
+    FlywayMigrationStrategy flywayMigrationStrategy(final JtaTransactionManager jtaTransactionManager) {
+        return Flyway::migrate;
+    }
 
     @Bean
     public RestTemplate restTemplate(ClientHttpRequestInterceptor... interceptors) {
