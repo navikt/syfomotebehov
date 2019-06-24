09b0fd0eb4101ba00931af932e48b3909ac67ead
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-06-24 10:16:19 +0200

    Bedre log-melding ved http 500 i controllere

--- src/main/java/no/nav/syfo/exception/ControllerExceptionHandler.java
+++ src/main/java/no/nav/syfo/exception/ControllerExceptionHandler.java
@@ -80,7 +80,7 @@ public class ControllerExceptionHandler {
         metrikk.tellHttpKall(status.value());
 
         if (HttpStatus.INTERNAL_SERVER_ERROR.equals(status)) {
-            log.error("Det skjedde en uventet feil: ", ex);
+            log.error("Uventet feil: {} : {}", ex.getClass().toString(), ex.getMessage(), ex);
             request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, WebRequest.SCOPE_REQUEST);
         }
 
