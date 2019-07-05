e02e76932b4fa3c8534e16cde32217f05ffdbd60
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-07-05 10:11:05 +0200

    Fjern sjekk av bruker geografiske tilknytning, alltid true mot syfoservice

--- src/main/java/no/nav/syfo/controller/GeografiskTilgangController.java
+++ src/main/java/no/nav/syfo/controller/GeografiskTilgangController.java
@@ -2,12 +2,10 @@ package no.nav.syfo.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import no.nav.security.spring.oidc.validation.api.ProtectedWithClaims;
-import no.nav.syfo.service.GeografiskTilgangService;
 import no.nav.syfo.util.Toggle;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
-import javax.validation.constraints.Pattern;
 import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.core.Response;
 
@@ -19,26 +17,15 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequestMapping(value = "/api/geografisktilgang")
 public class GeografiskTilgangController {
 
-    private GeografiskTilgangService geografiskTilgangService;
-
     @Inject
-    public GeografiskTilgangController(
-            final GeografiskTilgangService geografiskTilgangService
-    ) {
-        this.geografiskTilgangService = geografiskTilgangService;
+    public GeografiskTilgangController() {
     }
 
     @ProtectedWithClaims(issuer = INTERN, claimMap = {"sub=srvsyfoservice"})
     @GetMapping(produces = APPLICATION_JSON_VALUE)
-    public Response hentGeografiskTilgang(
-            @RequestParam(name = "fnr") @Pattern(regexp = "^[0-9]{11}$") String arbeidstakerFnr
-    ) {
+    public Response hentGeografiskTilgang() {
         if (Toggle.endepunkterForMotebehov) {
-            if (geografiskTilgangService.erMotebehovTilgjengelig(arbeidstakerFnr)) {
-                return Response.ok().build();
-            } else {
-                throw new ForbiddenException("Ikke tilgang");
-            }
+            return Response.ok().build();
         } else {
             log.info("Det ble gjort kall mot 'geografisktilgang', men dette endepunktet er togglet av.");
             throw new ForbiddenException("Ikke tilgang");
--- src/main/java/no/nav/syfo/util/Toggle.java
+++ src/main/java/no/nav/syfo/util/Toggle.java
@@ -8,18 +8,15 @@ public class Toggle {
 
     public static boolean enableNullstill;
     public static boolean endepunkterForMotebehov;
-    public static boolean enableMotebehovNasjonal;
     public static String pilotKontorer;
 
     public Toggle(
             @Value("${toggle.enable.nullstill:false}") boolean enableNullstill,
             @Value("${toggle.enable.motebehov:false}") boolean endepunkterForMotebehov,
-            @Value("${toggle.enable.motebehov.nasjonal:false}") boolean enableMotebehovNasjonal,
             @Value("${toggle.mvp.kontorer:}") String pilotKontorer
     ) {
         Toggle.enableNullstill = enableNullstill;
         Toggle.endepunkterForMotebehov = endepunkterForMotebehov;
-        Toggle.enableMotebehovNasjonal = enableMotebehovNasjonal;
         Toggle.pilotKontorer = pilotKontorer;
     }
 }
