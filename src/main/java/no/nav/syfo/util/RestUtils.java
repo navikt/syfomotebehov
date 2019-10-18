7318fca207f15e9b640d0d1184eadddb1d375884
John Martin Lindseth <john.martin.lindseth@nav.no>
2019-10-18 12:53:14 +0200

    Bugfix: Use correct link in veilederoppgaver

--- src/main/java/no/nav/syfo/util/RestUtils.java
+++ src/main/java/no/nav/syfo/util/RestUtils.java
@@ -4,16 +4,19 @@ import static java.lang.System.getenv;
 
 public class RestUtils {
 
+    private final static String NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME";
+    final static String DEV_FSS = "dev-fss";
+
     //TODO Hent fra Fasit via property i nais.yaml i stedet
     public static String baseUrl() {
         return "https://app" + miljo() + ".adeo.no";
     }
 
     private static String miljo() {
-        String environmentName = getenv("FASIT_ENVIRONMENT_NAME");
-        if ("p".equals(environmentName)) {
-            return "";
+        String environmentName = getenv(NAIS_CLUSTER_NAME);
+        if (DEV_FSS.equals(environmentName)) {
+            return "-q1";
         }
-        return "-" + environmentName;
+        return "";
     }
 }
