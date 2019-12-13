49f4a340db3232b11c958079ce5ec9e4d9a2a3fa
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-12-13 13:18:10 +0100

    Call syfotilgangskontroll with bearer token explicitly

--- src/main/java/no/nav/syfo/service/VeilederTilgangService.java
+++ src/main/java/no/nav/syfo/service/VeilederTilgangService.java
@@ -1,19 +1,27 @@
 package no.nav.syfo.service;
 
+import no.nav.security.oidc.context.OIDCRequestContextHolder;
 import no.nav.syfo.domain.rest.Fnr;
+import no.nav.syfo.oidc.OIDCIssuer;
+import no.nav.syfo.util.OIDCUtil;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Collections;
 
 import static java.util.Collections.singletonMap;
+import static no.nav.syfo.util.CredentialUtilKt.bearerCredentials;
 import static org.springframework.web.util.UriComponentsBuilder.fromHttpUrl;
 
 @Service
 public class VeilederTilgangService {
+
+    private final OIDCRequestContextHolder oidcContextHolder;
 
     public static final String FNR = "fnr";
     public static final String TILGANG_TIL_BRUKER_PATH = "/tilgangtilbruker";
@@ -24,7 +32,9 @@ public class VeilederTilgangService {
     private final UriComponentsBuilder tilgangTilBrukerViaAzureUriTemplate;
 
     public VeilederTilgangService(
-            @Value("${tilgangskontrollapi.url}") String tilgangskontrollUrl, RestTemplate template
+            @Value("${tilgangskontrollapi.url}") String tilgangskontrollUrl,
+            RestTemplate template,
+            OIDCRequestContextHolder oidcContextHolder
     ) {
         tilgangTilBrukerUriTemplate = fromHttpUrl(tilgangskontrollUrl)
                 .path(TILGANG_TIL_BRUKER_PATH)
@@ -33,6 +43,7 @@ public class VeilederTilgangService {
                 .path(TILGANG_TIL_BRUKER_VIA_AZURE_PATH)
                 .queryParam(FNR, FNR_PLACEHOLDER);
         this.template = template;
+        this.oidcContextHolder = oidcContextHolder;
     }
 
     public boolean sjekkVeiledersTilgangTilPerson(String fnr) {
@@ -42,7 +53,25 @@ public class VeilederTilgangService {
 
     public boolean sjekkVeiledersTilgangTilPersonViaAzure(Fnr fnr) {
         URI tilgangTilBrukerViaAzureUriMedFnr = tilgangTilBrukerViaAzureUriTemplate.build(singletonMap(FNR, fnr.getFnr()));
-        return kallUriMedTemplate(tilgangTilBrukerViaAzureUriMedFnr);
+        return checkAccess(tilgangTilBrukerViaAzureUriMedFnr, OIDCIssuer.AZURE);
+    }
+
+    private boolean checkAccess(URI uri, String oidcIssuer) {
+        try {
+            template.exchange(
+                    uri,
+                    HttpMethod.GET,
+                    createEntity(oidcIssuer),
+                    String.class
+            );
+            return true;
+        } catch (HttpClientErrorException e) {
+            if (e.getRawStatusCode() == 403) {
+                return false;
+            } else {
+                throw e;
+            }
+        }
     }
 
     private boolean kallUriMedTemplate(URI uri) {
@@ -58,4 +87,10 @@ public class VeilederTilgangService {
         }
     }
 
+    private HttpEntity<String> createEntity(String issuer) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.set("Authorization", bearerCredentials(OIDCUtil.tokenFraOIDC(oidcContextHolder, issuer)));
+        return new HttpEntity<>(headers);
+    }
 }
