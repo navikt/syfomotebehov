ab3cbc7f6e43dc2001404548ce5d7081dea55019
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-03-03 10:11:55 +0100

    Fix APP_CONSUMER_ID

--- src/main/kotlin/no/nav/syfo/util/RequestUtil.kt
+++ src/main/kotlin/no/nav/syfo/util/RequestUtil.kt
@@ -3,7 +3,7 @@ package no.nav.syfo.util
 import java.util.*
 
 const val NAV_CONSUMER_ID_HEADER = "Nav-Consumer-Id"
-const val APP_CONSUMER_ID = "syfooppfolgingsplanservice"
+const val APP_CONSUMER_ID = "syfomotebehov"
 const val NAV_CALL_ID_HEADER = "Nav-Call-Id"
 
 const val NAV_CONSUMER_TOKEN_HEADER = "Nav-Consumer-Token"
