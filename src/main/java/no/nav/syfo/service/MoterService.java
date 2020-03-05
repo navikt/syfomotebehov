26a2d74f18ea7b440928d6b665af44b57c5dcae4
Tor-Christian Tangenes <40393162+torchr89@users.noreply.github.com>
2020-03-05 08:17:15 +0100

    Include sts token in headers (#84)
    * Use STS-token as bearer token when calling syfomoteadmin REST-endpoint
    
    * Better error handling
    
    * Remove unnecessary nullable check, syfomoteadmin returns primitive type, which will lead to an exception in syfomoteadmin (500) in case of null

--- src/main/java/no/nav/syfo/service/MoterService.java
+++ src/main/java/no/nav/syfo/service/MoterService.java
@@ -1,10 +1,14 @@
 package no.nav.syfo.service;
 
 import lombok.extern.slf4j.Slf4j;
+import no.nav.syfo.sts.StsConsumer;
+import no.nav.syfo.util.Metrikk;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
-
 import java.time.LocalDateTime;
 
 import static java.util.Optional.ofNullable;
@@ -15,32 +19,55 @@ import static org.springframework.web.util.UriComponentsBuilder.fromHttpUrl;
 public class MoterService {
 
     private final RestTemplate template;
+    private final StsConsumer stsConsumer;
+    private final Metrikk metrikk;
     private String syfomoteadminUrl;
-
-    private static final String SYFOMOTEADMIN_FEILMELDING_NULL = "Fikk null fra syfomoteadmin ved henting av om arbeidstaker har mote i oppfolgingstilfellet";
     private static final String SYFOMOTEADMIN_FEILMELDING_GENERELL = "Klarte ikke hente om det er mote i oppfolgingstilfelle fra syfomoteadmin";
+    private static final String SYFOMOTEADMIN_FEILMELDING_KLIENT = "Fikk 4XX-feil ved henting av om det er mote i oppfolgingstilfelle fra syfomoteadmin";
+    private static final String SYFOMOTEADMIN_FEILMELDING_SERVER = "Fikk 5XX-feil ved henting av om det er mote i oppfolgingstilfelle fra syfomoteadmin";
 
     public MoterService(
             RestTemplate template,
+            StsConsumer stsConsumer,
+            Metrikk metrikk,
             @Value("${syfomoteadminapi.url}") String syfomoteadminUrl) {
         this.template = template;
+        this.stsConsumer = stsConsumer;
+        this.metrikk = metrikk;
         this.syfomoteadminUrl = syfomoteadminUrl;
     }
 
     boolean erMoteOpprettetForArbeidstakerEtterDato(String aktorId, LocalDateTime startDato) {
+        String stsToken = stsConsumer.token();
+        HttpEntity<LocalDateTime> requestEntity = new HttpEntity<>(startDato, authorizationHeader(stsToken));
+
         String url = fromHttpUrl(syfomoteadminUrl)
                 .pathSegment("system", aktorId, "harAktivtMote")
                 .toUriString();
 
         try {
-            Boolean erMoteOpprettetEtterDato = template.postForObject(url, startDato, Boolean.class);
-            if (!ofNullable(erMoteOpprettetEtterDato).isPresent()) {
-                throw new RuntimeException(SYFOMOTEADMIN_FEILMELDING_NULL);
-            }
+            metrikk.tellHendelse("call_syfomoteadmin");
+            Boolean erMoteOpprettetEtterDato = template.postForObject(url, requestEntity, Boolean.class);
+            metrikk.tellHendelse("call_syfomoteadmin_success");
             return erMoteOpprettetEtterDato;
-        } catch (Exception e) {
-            log.error("Det skjedde en feil ved henting av om det er mote i oppfolgingstilfelle fra syfomoteadmin", e);
-            throw new RuntimeException(SYFOMOTEADMIN_FEILMELDING_GENERELL, e);
+        } catch (HttpClientErrorException e) {
+            log.warn(SYFOMOTEADMIN_FEILMELDING_KLIENT);
+            throw e;
+        } catch (HttpServerErrorException e) {
+            metrikk.tellHendelse("call_syfomoteadmin_fail");
+            log.error(SYFOMOTEADMIN_FEILMELDING_SERVER, e);
+            throw e;
+        } catch (RuntimeException e) {
+            log.error(SYFOMOTEADMIN_FEILMELDING_GENERELL, e);
+            throw e;
         }
     }
+
+    private HttpHeaders authorizationHeader(String token) {
+        String bearerTokenCredentials = "Bearer " + token;
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, bearerTokenCredentials);
+        return headers;
+    }
+
 }
--- src/test/java/no/nav/syfo/service/MoterServiceTest.java
+++ src/test/java/no/nav/syfo/service/MoterServiceTest.java
@@ -1,17 +1,20 @@
 package no.nav.syfo.service;
 
+import no.nav.syfo.sts.StsConsumer;
+import no.nav.syfo.util.Metrikk;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpEntity;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
-
+import org.springframework.web.client.HttpServerErrorException;
 import java.time.LocalDateTime;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static junit.framework.TestCase.assertFalse;
 import static no.nav.syfo.testhelper.UserConstants.ARBEIDSTAKER_AKTORID;
 import static org.junit.Assert.assertTrue;
@@ -21,6 +24,12 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MoterServiceTest {
+
+    @Mock
+    private StsConsumer stsConsumer;
+
+    @Mock
+    private Metrikk metrikk;
 
     @Mock
     private RestTemplate template;
@@ -37,28 +46,21 @@ public class MoterServiceTest {
 
     @Test
     public void harArbeidstakerMoteIOppfolgingstilfelle_skal_returnere_true_hvis_moteplanlegger_er_brukt() {
-        when(template.postForObject(anyString(), any(LocalDateTime.class), any())).thenReturn(true);
+        when(template.postForObject(anyString(), any(HttpEntity.class), any())).thenReturn(true);
 
         assertTrue(moterService.erMoteOpprettetForArbeidstakerEtterDato(ARBEIDSTAKER_AKTORID, START_DATO));
     }
 
     @Test
     public void harArbeidstakerMoteIOppfolgingstilfelle_skal_returnere_false_hvis_moteplanlegger_ikke_er_brukt() {
-        when(template.postForObject(anyString(), any(LocalDateTime.class), any())).thenReturn(false);
+        when(template.postForObject(anyString(), any(HttpEntity.class), any())).thenReturn(false);
 
         assertFalse(moterService.erMoteOpprettetForArbeidstakerEtterDato(ARBEIDSTAKER_AKTORID, START_DATO));
     }
 
-    @Test(expected = RuntimeException.class)
-    public void harArbeidstakerMoteIOppfolgingstilfelle_skal_kaste_exception_hvis_svar_fra_syfomoteadmin_er_null() {
-        when(template.postForObject(anyString(), any(LocalDateTime.class), any())).thenReturn(null);
-
-        moterService.erMoteOpprettetForArbeidstakerEtterDato(ARBEIDSTAKER_AKTORID, START_DATO);
-    }
-
-    @Test(expected = RuntimeException.class)
-    public void harArbeidstakerMoteIOppfolgingstilfelle_skal_kaste_exception_hvis_kall_til_syfomoteadmin_feiler() {
-        when(template.postForObject(anyString(), any(LocalDateTime.class), any())).thenReturn(HttpStatus.INTERNAL_SERVER_ERROR);
+    @Test(expected = HttpServerErrorException.class)
+    public void harArbeidstakerMoteIOppfolgingstilfelle_skal_kaste_HttpServerErrorException_hvis_kall_til_syfomoteadmin_feiler() {
+        when(template.postForObject(anyString(), any(HttpEntity.class), any())).thenThrow(new HttpServerErrorException(INTERNAL_SERVER_ERROR));
 
         moterService.erMoteOpprettetForArbeidstakerEtterDato(ARBEIDSTAKER_AKTORID, START_DATO);
     }
