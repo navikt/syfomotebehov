41317349b803fb8d59f45593c4ade5212d655da3
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-04-21 14:13:05 +0200

    Create database table Oppfolgingstilfelle

--- src/main/java/no/nav/syfo/config/ApplicationConfig.java
+++ src/main/java/no/nav/syfo/config/ApplicationConfig.java
@@ -1,15 +1,11 @@
 package no.nav.syfo.config;
 
-import org.flywaydb.core.Flyway;
-import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.jta.JtaTransactionManager;
 import org.springframework.web.client.RestTemplate;
 
 import static java.util.Arrays.asList;
@@ -18,13 +14,6 @@ import static java.util.Arrays.asList;
 @EnableTransactionManagement
 @EnableScheduling
 public class ApplicationConfig {
-
-    // Sørger for at flyway migrering skjer etter at JTA transaction manager er ferdig satt opp av Spring.
-    // Forhindrer WARNING: transaction manager not running? loggspam fra Atomikos.
-    @Bean
-    FlywayMigrationStrategy flywayMigrationStrategy(final JtaTransactionManager jtaTransactionManager) {
-        return Flyway::migrate;
-    }
 
     @Bean
     public TaskScheduler taskScheduler() {
