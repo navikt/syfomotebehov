681da12d2291e61f458db3fcc1420c8eac9eef74
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-05-05 13:42:44 +0200

    Add endpoint for syfoservice to check availability for Varsel to Arbeidstaker

---
+++ src/main/kotlin/no/nav/syfo/varsel/MotebehovsvarVarselInfoArbeidstaker.kt
@@ -0,0 +1,7 @@
+package no.nav.syfo.varsel
+
+import java.io.Serializable
+
+data class MotebehovsvarVarselInfoArbeidstaker(
+        val sykmeldtAktorId: String
+) : Serializable
--- src/main/kotlin/no/nav/syfo/varsel/api/VarselController.kt
+++ src/main/kotlin/no/nav/syfo/varsel/api/VarselController.kt
@@ -1,10 +1,16 @@
 package no.nav.syfo.varsel.api
 
 import no.nav.security.oidc.api.ProtectedWithClaims
-import no.nav.syfo.varsel.MotebehovsvarVarselInfo
 import no.nav.syfo.api.auth.OIDCIssuer.INTERN
+import no.nav.syfo.consumer.aktorregister.AktorregisterConsumer
+import no.nav.syfo.consumer.aktorregister.domain.AktorId
+import no.nav.syfo.consumer.aktorregister.domain.Fodselsnummer
+import no.nav.syfo.motebehov.motebehovstatus.MotebehovStatusService
+import no.nav.syfo.varsel.MotebehovsvarVarselInfo
+import no.nav.syfo.varsel.MotebehovsvarVarselInfoArbeidstaker
 import no.nav.syfo.varsel.VarselService
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.inject.Inject
 import javax.ws.rs.core.Response
@@ -12,6 +18,8 @@ import javax.ws.rs.core.Response
 @RestController
 @RequestMapping(value = ["/api/varsel"])
 class VarselController @Inject constructor(
+        private val aktorregisterConsumer: AktorregisterConsumer,
+        private val motebehovStatusService: MotebehovStatusService,
         private val varselService: VarselService
 ) {
     @ResponseBody
@@ -24,5 +32,18 @@ class VarselController @Inject constructor(
         return Response
                 .ok()
                 .build()
+    }
+
+    @ResponseBody
+    @ProtectedWithClaims(issuer = INTERN, claimMap = ["sub=srvsyfoservice"])
+    @PostMapping(value = ["/availability/arbeidstaker"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun motebehovVarselAvailabilityArbeidstaker(
+            @RequestBody motebehovsvarVarselInfo: MotebehovsvarVarselInfoArbeidstaker
+    ): ResponseEntity<Boolean> {
+        val arbeidstakerFnr = aktorregisterConsumer.getFnrForAktorId(AktorId(motebehovsvarVarselInfo.sykmeldtAktorId))
+        val isVarselAvailableForMotebehov = motebehovStatusService.motebehovStatusForArbeidstaker(Fodselsnummer(arbeidstakerFnr)).visMotebehov
+        return ResponseEntity
+                .ok()
+                .body(isVarselAvailableForMotebehov)
     }
 }
