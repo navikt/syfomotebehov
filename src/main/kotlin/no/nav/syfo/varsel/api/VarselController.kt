4f9a85672eeb16435cf0f4d72878fce4e6ff73fc
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-05-05 12:54:45 +0200

    Refactor requetmapping VarselController

--- src/main/kotlin/no/nav/syfo/varsel/api/VarselController.kt
+++ src/main/kotlin/no/nav/syfo/varsel/api/VarselController.kt
@@ -10,13 +10,13 @@ import javax.inject.Inject
 import javax.ws.rs.core.Response
 
 @RestController
-@RequestMapping(value = ["/api/varsel/naermesteleder"])
+@RequestMapping(value = ["/api/varsel"])
 class VarselController @Inject constructor(
         private val varselService: VarselService
 ) {
     @ResponseBody
     @ProtectedWithClaims(issuer = INTERN, claimMap = ["sub=srvsyfoservice"])
-    @PostMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    @PostMapping(value = ["/naermesteleder"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun sendVarselNaermesteLeder(
             @RequestBody motebehovsvarVarselInfo: MotebehovsvarVarselInfo
     ): Response {
