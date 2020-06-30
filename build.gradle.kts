571bd4841aa4fb41508d7a75f3615ff0deb45bfb
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-06-30 09:11:40 +0200

    Bump shadowJar 5.2.0 -> 6.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
     kotlin("jvm") version "1.3.72"
     id("java")
     id("org.jetbrains.kotlin.plugin.allopen") version "1.3.50"
-    id("com.github.johnrengelman.shadow") version "5.2.0"
+    id("com.github.johnrengelman.shadow") version "6.0.0"
     id("org.springframework.boot") version "2.2.7.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
