987799f12672759e27dde2bfaef54867e30c1e7a
Tor-Christian Tangenes <torchrct@gmail.com>
2022-05-11 10:22:53 +0200

    Lint

--- src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/kafka/KafkaOppfolgingstilfelleConfig.kt
+++ src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/kafka/KafkaOppfolgingstilfelleConfig.kt
@@ -1,5 +1,6 @@
 package no.nav.syfo.oppfolgingstilfelle.kafka
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.*
@@ -8,7 +9,6 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 import org.springframework.kafka.listener.ContainerProperties
-import java.io.File
 
 @Profile("remote")
 @Configuration
@@ -17,11 +17,11 @@ class KafkaOppfolgingstilfelleConfig(
     @Value("\${kafka.truststore.path}") val truststorePath: String,
     @Value("\${kafka.keystore.path}") val keystorePath: String,
     @Value("\${kafka.credstore.password}") val credstorePassword: String
-){
+) {
     @Bean
     fun oppfolgingstilfconsumerFactory(properties: KafkaProperties): ConsumerFactory<String, String> {
         val strlist = kafkaBrokers.split(",")
-        //log.info("KAFKA_BROKERS_RAW: $kafkaBrokers | list: $strlist")
+        log.info("KAFKA_BROKERS_RAW: $kafkaBrokers | list: $strlist")
         properties.security.protocol = "SSL"
         properties.ssl.trustStoreType = "JAVA_STORE"
         properties.ssl.keyStoreType = "PKCS12"
@@ -31,7 +31,6 @@ class KafkaOppfolgingstilfelleConfig(
         properties.ssl.keyStorePassword = credstorePassword
         properties.ssl.keyPassword = credstorePassword
         properties.bootstrapServers = kafkaBrokers.split(",")
-
 
         return DefaultKafkaConsumerFactory(properties.buildConsumerProperties())
     }
@@ -43,5 +42,8 @@ class KafkaOppfolgingstilfelleConfig(
             this.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL_IMMEDIATE
             this.consumerFactory = oppfolgingstilfconsumerFactory
         }
+    }
+    companion object {
+        private val log = LoggerFactory.getLogger(KafkaOppfolgingstilfelleConfig::class.java)
     }
 }
