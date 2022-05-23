279342866267887fa2d18c3071412cb3109f1e62
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2022-05-23 14:58:06 +0200

    Delete unused param

--- src/main/kotlin/no/nav/syfo/varsel/esyfovarsel/EsyfovarselPlanleggingService.kt
+++ src/main/kotlin/no/nav/syfo/varsel/esyfovarsel/EsyfovarselPlanleggingService.kt
@@ -8,11 +8,11 @@ import java.time.LocalDate
 @Service
 class EsyfovarselPlanleggingService(private val producer: EsyfovarselPlanleggingProducer) {
 
-    fun planleggSvarMotebehovVarselTilNarmesteLeder(varselDato: LocalDate, arbeidstakerFnr: String, ansattFnr: String, orgnummer: String) {
+    fun planleggSvarMotebehovVarselTilNarmesteLeder(varselDato: LocalDate, arbeidstakerFnr: String, orgnummer: String) {
         planleggVarsel(varselDato, arbeidstakerFnr, orgnummer, HendelseType.NL_DIALOGMOTE_SVAR_MOTEBEHOV)
     }
 
-    fun planleggSvarMotebehovVarselTilArbeidstaker(varselDato: LocalDate, arbeidstakerFnr: String, ansattFnr: String, orgnummer: String) {
+    fun planleggSvarMotebehovVarselTilArbeidstaker(varselDato: LocalDate, arbeidstakerFnr: String, orgnummer: String) {
         planleggVarsel(varselDato, arbeidstakerFnr, orgnummer, HendelseType.SM_DIALOGMOTE_SVAR_MOTEBEHOV)
     }
 
