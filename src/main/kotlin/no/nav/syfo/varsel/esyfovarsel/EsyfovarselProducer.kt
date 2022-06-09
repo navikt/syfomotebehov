4dda1d3552e91742342d7ec1682976613009e771
Kaja Kvinge Akselsen <75254007+kaja-nav@users.noreply.github.com>
2022-06-09 14:31:45 +0200

    Revert "Send to varsel-planlegging instead of varselbus. Add support for sykmeldt (#168)" (#171)
    This reverts commit bd06c905237d60e41a96353acfec5583d4bb6028.

---
+++ src/main/kotlin/no/nav/syfo/varsel/esyfovarsel/EsyfovarselProducer.kt
@@ -0,0 +1,37 @@
+package no.nav.syfo.varsel.esyfovarsel
+
+import no.nav.syfo.varsel.esyfovarsel.domain.EsyfovarselHendelse
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class EsyfovarselProducer @Autowired constructor(
+    @Qualifier("EsyfovarselKafkaTemplate") private val kafkaTemplate: KafkaTemplate<String, EsyfovarselHendelse>,
+) {
+    fun sendVarselTilEsyfovarsel(
+        esyfovarselHendelse: EsyfovarselHendelse,
+    ) {
+        try {
+            kafkaTemplate.send(
+                ProducerRecord(
+                    ESYFOVARSEL_TOPIC,
+                    UUID.randomUUID().toString(),
+                    esyfovarselHendelse,
+                )
+            ).get()
+        } catch (e: Exception) {
+            log.error("Exception was thrown when attempting to send varsel to esyfovarsel. ${e.message}")
+            throw e
+        }
+    }
+
+    companion object {
+        private const val ESYFOVARSEL_TOPIC = "team-esyfo.varselbus"
+        private val log = LoggerFactory.getLogger(EsyfovarselProducer::class.java)
+    }
+}
