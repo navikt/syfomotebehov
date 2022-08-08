881a3ebd9f6938c0cc2c2e7c8bb842de6ac20a6a
Tanja Krasnomovets <Vitenok@users.noreply.github.com>
2022-08-08 13:41:48 +0200

    Revert "Revert "Add support for tokenX motebehov" (#187)" (#188)
    This reverts commit 08be6421aad4ecb3ded6b3585156743e5d02a62d.

---
+++ src/main/kotlin/no/nav/syfo/api/auth/tokenX/TokenXUtil.kt
@@ -0,0 +1,48 @@
+package no.nav.syfo.api.auth.tokenX
+
+import javax.ws.rs.ForbiddenException
+import no.nav.security.token.support.core.context.TokenValidationContextHolder
+import no.nav.security.token.support.core.jwt.JwtTokenClaims
+import no.nav.syfo.consumer.aktorregister.domain.Fodselsnummer
+
+object TokenXUtil {
+    @Throws(ForbiddenException::class)
+    fun validateTokenXClaims(
+        contextHolder: TokenValidationContextHolder,
+        requestedClientId: String,
+        requestedTokenxIdp: String
+    ): JwtTokenClaims {
+        val context = contextHolder.tokenValidationContext
+        val claims = context.getClaims(TokenXIssuer.TOKENX)
+        val clientId = claims.getStringClaim("client_id")
+
+        if (clientId != requestedClientId) {
+            throw ForbiddenException("Uventet client id $clientId")
+        }
+        val idp = claims.getStringClaim("idp")
+        if (idp != requestedTokenxIdp) {
+            // Check that  Idporten was IDP for tokenX
+            throw ForbiddenException("Uventet idp $idp, requestedTokenxIdp: $requestedTokenxIdp")
+        }
+        return claims
+    }
+
+    fun JwtTokenClaims.fnrFromIdportenTokenX(): Fodselsnummer {
+        return Fodselsnummer(this.getStringClaim("pid"))
+    }
+
+    fun fnrFromIdportenTokenX(contextHolder: TokenValidationContextHolder): Fodselsnummer {
+        val context = contextHolder.tokenValidationContext
+        val claims = context.getClaims(TokenXIssuer.TOKENX)
+        return Fodselsnummer(claims.getStringClaim("pid"))
+    }
+
+    fun tokenFromTokenX(contextHolder: TokenValidationContextHolder): String {
+        val context = contextHolder.tokenValidationContext
+        return context.getJwtToken(TokenXIssuer.TOKENX).tokenAsString
+    }
+
+    object TokenXIssuer {
+        const val TOKENX = "tokenx"
+    }
+}
---
+++ src/main/kotlin/no/nav/syfo/consumer/tokenx/tokendings/TokenDingsConsumer.kt
@@ -0,0 +1,121 @@
+package no.nav.syfo.consumer.tokenx.tokendings
+
+import com.nimbusds.jose.JOSEObjectType
+import com.nimbusds.jose.JWSAlgorithm
+import com.nimbusds.jose.JWSHeader
+import com.nimbusds.jose.crypto.RSASSASigner
+import com.nimbusds.jose.jwk.RSAKey
+import com.nimbusds.jwt.JWTClaimsSet
+import com.nimbusds.jwt.SignedJWT
+import no.nav.syfo.consumer.tokenx.TokenXResponse
+import no.nav.syfo.consumer.tokenx.TokenXToken
+import no.nav.syfo.consumer.tokenx.isExpired
+import no.nav.syfo.consumer.tokenx.toTokenXToken
+import no.nav.syfo.consumer.tokenx.tokendings.metadata.TokenDingsMetadataConsumer
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
+import org.springframework.web.client.RestClientResponseException
+import org.springframework.web.client.RestTemplate
+import java.time.Instant
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+import javax.inject.Inject
+
+@Component
+class TokenDingsConsumer @Inject constructor(
+    private val restTemplate: RestTemplate,
+    private val tokenDingsMetadataConsumer: TokenDingsMetadataConsumer,
+    @Value("\${token.x.client.id}") private val clientId: String,
+    @Value("\${token.x.private.jwk}") private val privateJwk: String,
+) {
+    fun exchangeToken(
+        subjectToken: String,
+        targetApp: String
+    ): String {
+        val tokendingsEndpoint = tokenDingsMetadataConsumer.getTokenDingsMetadata().tokenEndpoint
+        val requestEntity = requestEntity(subjectToken, tokendingsEndpoint, targetApp)
+
+        log.warn("requestEntity: $requestEntity")
+        log.warn("wellKnownUrl: $tokendingsEndpoint")
+
+        val cachedToken = tokenXCache[targetApp]
+
+        return if (cachedToken?.isExpired() == false) {
+            cachedToken.accessToken
+        } else {
+            try {
+                val response = restTemplate.exchange(
+                    tokendingsEndpoint,
+                    HttpMethod.POST,
+                    requestEntity,
+                    TokenXResponse::class.java
+                )
+                val tokenX = response.body!!.toTokenXToken()
+                tokenXCache[targetApp] = tokenX
+
+                return tokenX.accessToken
+            } catch (e: RestClientResponseException) {
+                log.error(
+                    "Call to get TokenX failed with status: ${e.rawStatusCode} and message: ${e.responseBodyAsString}",
+                    e
+                )
+                throw e
+            }
+        }
+    }
+
+    private fun requestEntity(
+        subjectToken: String,
+        tokenEndpoint: String,
+        targetApp: String,
+    ): HttpEntity<MultiValueMap<String, String>> {
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_FORM_URLENCODED
+        val body: MultiValueMap<String, String> = LinkedMultiValueMap()
+        body.add("grant_type", "urn:ietf:params:oauth:grant-type:token-exchange")
+        body.add("client_assertion_type", "urn:ietf:params:oauth:client-assertion-type:jwt-bearer")
+        body.add("client_assertion", getClientAssertion(tokenEndpoint))
+        body.add("subject_token_type", "urn:ietf:params:oauth:token-type:jwt")
+        body.add("subject_token", subjectToken)
+        body.add("audience", targetApp)
+        return HttpEntity(body, headers)
+    }
+
+    fun getClientAssertion(tokenEndpoint: String): String {
+        val rsaKey = RSAKey.parse(privateJwk)
+        val now = Date.from(Instant.now())
+        return JWTClaimsSet.Builder()
+            .issuer(clientId)
+            .subject(clientId)
+            .audience(tokenEndpoint)
+            .issueTime(now)
+            .expirationTime(Date.from(Instant.now().plusSeconds(60)))
+            .jwtID(UUID.randomUUID().toString())
+            .notBeforeTime(now)
+            .build()
+            .sign(rsaKey)
+            .serialize()
+    }
+
+    companion object {
+        private val tokenXCache = ConcurrentHashMap<String, TokenXToken>()
+        private val log = LoggerFactory.getLogger(TokenDingsConsumer::class.java)
+    }
+}
+
+internal fun JWTClaimsSet.sign(rsaKey: RSAKey): SignedJWT =
+    SignedJWT(
+        JWSHeader.Builder(JWSAlgorithm.RS256)
+            .keyID(rsaKey.keyID)
+            .type(JOSEObjectType.JWT).build(),
+        this
+    ).apply {
+        sign(RSASSASigner(rsaKey.toPrivateKey()))
+    }
