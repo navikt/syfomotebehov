f859d1d08aeb269f77abe33ddd67a74691ab4852
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2022-08-23 09:47:11 +0200

    Refactor esyfovarsel types

--- src/main/kotlin/no/nav/syfo/varsel/esyfovarsel/domain/EsyfovarselDTO.kt
+++ src/main/kotlin/no/nav/syfo/varsel/esyfovarsel/domain/EsyfovarselDTO.kt
@@ -1,21 +1,40 @@
 package no.nav.syfo.varsel.esyfovarsel.domain
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import java.io.Serializable
 
-data class EsyfovarselHendelse(
-    val mottakerFnr: String,
-    val type: HendelseType,
-    val data: EsyfovarselHendelseData?
-) : Serializable
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME)
+sealed interface EsyfovarselHendelse : Serializable {
+    val mottakerFnr: String
+    val type: HendelseType
+    var data: Any?
+}
+
+data class NarmesteLederHendelse(
+    override val mottakerFnr: String, // To be removed
+    override val type: HendelseType,
+    override var data: Any?,
+    val narmesteLederFnr: String,
+    val arbeidstakerFnr: String,
+    val orgnummer: String
+) : EsyfovarselHendelse
+
+data class ArbeidstakerHendelse(
+    override val mottakerFnr: String, // To be removed
+    override val type: HendelseType,
+    override var data: Any?,
+    val arbeidstakerFnr: String,
+    val orgnummer: String?
+) : EsyfovarselHendelse
 
 enum class HendelseType {
     NL_DIALOGMOTE_SVAR_MOTEBEHOV,
     SM_DIALOGMOTE_SVAR_MOTEBEHOV,
 }
 
-interface EsyfovarselHendelseData : Serializable
+interface EsyfovarselHendelseData : Serializable // To be removed
 
-data class NarmesteLederVarselData(
+data class NarmesteLederVarselData( // To be removed
     val ansattFnr: String,
     val orgnummer: String
 ) : EsyfovarselHendelseData
