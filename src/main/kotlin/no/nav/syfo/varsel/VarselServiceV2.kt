eb98f4e7ba6942f0936fe333f9cb747bfb76eaa5
AudunSorheim <audun.sorheim@nav.no>
2022-09-28 13:16:12 +0200

    Log count of virksomhetsnummer

--- src/main/kotlin/no/nav/syfo/varsel/VarselServiceV2.kt
+++ src/main/kotlin/no/nav/syfo/varsel/VarselServiceV2.kt
@@ -43,7 +43,9 @@ class VarselServiceV2 @Inject constructor(
         log.info("Testing: Henter nærmeste ledere..")
         val narmesteLederRelations = narmesteLederService.getAllNarmesteLederRelations(ansattFnr)
 
-        log.info("Antall unike nærmeste ledere for kandidatUuid $kandidatUuid: ${narmesteLederRelations?.size ?: 0}")
+        val amountOfVirksomheter = narmesteLederRelations?.distinctBy { it.virksomhetsnummer }?.size ?: 0
+
+        log.info("Antall unike nærmeste ledere for kandidatUuid $kandidatUuid: ${narmesteLederRelations?.size ?: 0}, antall virksomheter: $amountOfVirksomheter")
 
         log.info("Testing: Sender varsel til arbeidstaker")
         sendVarselTilArbeidstaker(ansattFnr, ansattesOppfolgingstilfelle)
