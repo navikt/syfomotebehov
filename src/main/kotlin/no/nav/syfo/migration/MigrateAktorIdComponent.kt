a86879d56724c9b0776559688147c8bb35d95611
Tor-Christian Tangenes <torchrct@gmail.com>
2022-11-21 07:47:32 +0100

    Revert "Change cron schedule for migration-job (#230)"
    This reverts commit 82c17e3766c4518564f7e9aa4f77c471df92dcb9.

--- src/main/kotlin/no/nav/syfo/migration/MigrateAktorIdComponent.kt
+++ src/main/kotlin/no/nav/syfo/migration/MigrateAktorIdComponent.kt
@@ -11,7 +11,7 @@ class MigrateAktorIdComponent(
     private val motebehovDAO: MotebehovDAO,
     private val pdlConsumer: PdlConsumer
 ) {
-    @Scheduled(cron = "0 */30 * * * *")
+    @Scheduled(cron = "0 */15 * * * *")
     fun run() {
         log.info("Running migration job")
         val motebehovUtenFnr = motebehovDAO.hentMotebehovUtenFnr()
