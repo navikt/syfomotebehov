696e339af5187cb8d99cd8953691e713f3a43e4f
Vitenok <victorandreevi4@gmail.com>
2022-12-12 06:30:30 +0100

    Cleanup EsyfovarselKafkaConfig.kt. Delete unused EsyfovarselConsumer.kt. Delete empty java folder

---
+++ src/main/kotlin/no/nav/syfo/varsel/esyfovarsel/KafkaEsyfovarselConfig.kt
@@ -0,0 +1,30 @@
+package no.nav.syfo.varsel.esyfovarsel
+
+import no.nav.syfo.config.kafka.KafkaAivenConfig
+import no.nav.syfo.varsel.esyfovarsel.domain.EsyfovarselHendelse
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+
+@EnableKafka
+@Configuration
+class KafkaEsyfovarselConfig(
+    private val kafkaAivenConfig: KafkaAivenConfig,
+) {
+    @Bean("EsyfovarselProducerFactory")
+    fun producerFactory(): ProducerFactory<String, EsyfovarselHendelse> {
+
+        return DefaultKafkaProducerFactory(
+            kafkaAivenConfig.commonKafkaAivenProducerConfig(),
+        )
+    }
+
+    @Bean("EsyfovarselKafkaTemplate")
+    fun kafkaTemplate(@Qualifier("EsyfovarselProducerFactory") producerFactory: ProducerFactory<String, EsyfovarselHendelse>): KafkaTemplate<String, EsyfovarselHendelse> {
+        return KafkaTemplate(producerFactory)
+    }
+}
