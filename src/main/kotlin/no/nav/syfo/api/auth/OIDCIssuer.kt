f35f0dcd4c3928a6a4ef3b60ea8df72bf8f0f9c9
Luan Tran <8613937+luantr@users.noreply.github.com>
2023-02-17 11:52:04 +0100

    Remove isso token validator rule (#237)

--- src/main/kotlin/no/nav/syfo/api/auth/OIDCIssuer.kt
+++ src/main/kotlin/no/nav/syfo/api/auth/OIDCIssuer.kt
@@ -1,8 +1,6 @@
 package no.nav.syfo.api.auth
 
 object OIDCIssuer {
-    const val STS = "sts"
-    const val INTERN = "intern"
     const val EKSTERN = "selvbetjening"
     const val INTERN_AZUREAD_V2 = "internazureadv2"
 }
