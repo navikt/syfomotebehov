4e18a98fadbc84d5bc49d64dc9eadc41d3ec23c4
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2023-03-24 13:24:42 +0100

    Remove usage of EKSTERN

--- src/main/kotlin/no/nav/syfo/api/auth/OIDCIssuer.kt
+++ src/main/kotlin/no/nav/syfo/api/auth/OIDCIssuer.kt
@@ -1,6 +1,5 @@
 package no.nav.syfo.api.auth
 
 object OIDCIssuer {
-    const val EKSTERN = "selvbetjening"
     const val INTERN_AZUREAD_V2 = "internazureadv2"
 }
--- src/main/kotlin/no/nav/syfo/api/auth/OIDCUtil.kt
+++ src/main/kotlin/no/nav/syfo/api/auth/OIDCUtil.kt
@@ -2,17 +2,10 @@ package no.nav.syfo.api.auth
 
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
 object OIDCUtil {
-    const val PID_CLAIM_NAME = "pid"
 
     fun tokenFraOIDC(contextHolder: TokenValidationContextHolder, issuer: String?): String {
         val context = contextHolder.tokenValidationContext
         return context.getJwtToken(issuer).tokenAsString
-    }
-
-    fun fnrFraOIDCEkstern(contextHolder: TokenValidationContextHolder): String {
-        val context = contextHolder.tokenValidationContext
-        val jwtTokenClaims = context.getClaims(OIDCIssuer.EKSTERN)
-        return jwtTokenClaims.getStringClaim(PID_CLAIM_NAME)
     }
 }
 
