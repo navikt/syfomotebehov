84e4d9e503a2f8157cf0fc44f12ec2440626fe74
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2023-05-15 15:52:29 +0200

    Fix linting error

--- src/main/kotlin/no/nav/syfo/motebehov/MotebehovOppfolgingstilfelleServiceV2.kt
+++ src/main/kotlin/no/nav/syfo/motebehov/MotebehovOppfolgingstilfelleServiceV2.kt
@@ -44,7 +44,7 @@ class MotebehovOppfolgingstilfelleServiceV2 @Inject constructor(
                     nyttMotebehov.motebehovSvar,
                     false
                 )
-                if(motebehovStatus.skjemaType == MotebehovSkjemaType.SVAR_BEHOV) {
+                if (motebehovStatus.skjemaType == MotebehovSkjemaType.SVAR_BEHOV) {
                     varselServiceV2.ferdigstillSvarMotebehovVarselForNaermesteLeder(arbeidstakerFnr, innloggetFnr, nyttMotebehov.virksomhetsnummer)
                 }
             } else {
@@ -87,7 +87,7 @@ class MotebehovOppfolgingstilfelleServiceV2 @Inject constructor(
                     motebehovSvar,
                     true
                 )
-                if(motebehovStatus.skjemaType == MotebehovSkjemaType.SVAR_BEHOV) {
+                if (motebehovStatus.skjemaType == MotebehovSkjemaType.SVAR_BEHOV) {
                     varselServiceV2.ferdigstillSvarMotebehovVarselForArbeidstaker(arbeidstakerFnr)
                 }
             } else {
