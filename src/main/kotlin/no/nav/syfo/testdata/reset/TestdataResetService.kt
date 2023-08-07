7c7d99ce54f61ef47c5c83e450bc068908312d1d
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2023-08-07 15:19:26 +0200

    Delete oppfolgingstilfeller for user

--- src/main/kotlin/no/nav/syfo/testdata/reset/TestdataResetService.kt
+++ src/main/kotlin/no/nav/syfo/testdata/reset/TestdataResetService.kt
@@ -4,6 +4,7 @@ import no.nav.syfo.consumer.pdl.PdlConsumer
 import no.nav.syfo.dialogmote.database.DialogmoteDAO
 import no.nav.syfo.dialogmotekandidat.database.DialogmotekandidatDAO
 import no.nav.syfo.motebehov.database.MotebehovDAO
+import no.nav.syfo.oppfolgingstilfelle.database.OppfolgingstilfelleDAO
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import javax.inject.Inject
@@ -13,15 +14,17 @@ class TestdataResetService @Inject constructor(
     private val motebehovDAO: MotebehovDAO,
     private val dialogmoteDAO: DialogmoteDAO,
     private val dialogmotekandidatDAO: DialogmotekandidatDAO,
+    private val oppfolgingstilfelleDAO: OppfolgingstilfelleDAO,
     private val pdlConsumer: PdlConsumer,
 ) {
 
     fun resetTestdata(fnr: String) {
         val aktoerId = pdlConsumer.aktorid(fnr)
-        log.info("Nullstiller møtebehov, dialogmøter, kandidat for fnr $fnr")
+        log.info("Nullstiller møtebehov, dialogmøter, kandidat og oppfølgingstilfeller for fnr $fnr")
         motebehovDAO.nullstillMotebehov(aktoerId)
         dialogmoteDAO.nullstillDialogmoter(fnr)
         dialogmotekandidatDAO.delete(fnr)
+        oppfolgingstilfelleDAO.nullstillOppfolgingstilfeller(fnr)
     }
 
     companion object {
