95b7f45725c866c1a9fb7ae555121d0a375f2db8
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2023-10-03 12:43:39 +0200

    Remove spring profile

--- src/main/kotlin/no/nav/syfo/motebehov/scheduler/BehandleUbehandleteMotebehovScheduler.kt
+++ src/main/kotlin/no/nav/syfo/motebehov/scheduler/BehandleUbehandleteMotebehovScheduler.kt
@@ -3,14 +3,12 @@ package no.nav.syfo.motebehov.scheduler
 import no.nav.syfo.leaderelection.LeaderElectionClient
 import no.nav.syfo.motebehov.MotebehovService
 import org.slf4j.LoggerFactory
-import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.time.LocalDate
 import javax.inject.Inject
 
 @Component
-@Profile("behandle_ubehandlede_motebehov")
 class BehandleUbehandleteMotebehovScheduler @Inject constructor(
     private val leaderElectionClient: LeaderElectionClient,
     private val motebehovService: MotebehovService,
