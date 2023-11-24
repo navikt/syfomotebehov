349bd7e921cfaf87edea0bda2714f9c37e3ddfb1
Tor-Christian Tangenes <torchrct@gmail.com>
2023-11-24 15:45:01 +0100

    Bump spring boot to 3.2 to get rid of snakeyaml vulnerability

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ plugins {
     kotlin("jvm") version "1.9.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.9.0"
-    id("org.springframework.boot") version "3.1.5"
+    id("org.springframework.boot") version "3.2.0"
     id("io.spring.dependency-management") version "1.1.4"
     id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
 }
