d661427073b5b73b590cace4bfdb5014f9fa890d
geir-waagboe <geir.arne.waagbo@nav.no>
2024-01-12 09:22:54 +0100

    Adjust grants after migration and give read access to DVH

--- src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
+++ src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
@@ -1,25 +1,3 @@
 REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
 
--- GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
-GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO cloudsqliamuser;
-
-DO $$
-BEGIN
-  CREATE ROLE cloudsqlsuperuser WITH NOLOGIN;
-  EXCEPTION WHEN DUPLICATE_OBJECT THEN
-  RAISE NOTICE 'not creating role cloudsqlsuperuser -- it already exists';
-END
-$$;
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO cloudsqlsuperuser;
-GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO cloudsqlsuperuser;
-
-DO $$
-BEGIN
-  CREATE USER "esyfo-analyse";
-  EXCEPTION WHEN DUPLICATE_OBJECT THEN
-  RAISE NOTICE 'not creating role esyfo-analyse -- it already exists';
-END
-$$;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO "esyfo-analyse";
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
---
+++ src/main/resources/db/migration/V1_6__grant_dvh.sql
@@ -0,0 +1,1 @@
+GRANT SELECT ON motebehov to "disykefravar-x4wt@knada-gcp.iam.gserviceaccount.com";
