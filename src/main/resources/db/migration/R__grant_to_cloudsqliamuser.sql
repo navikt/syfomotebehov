91387bc25417f2f18e9c53f0abbca2cbeb1efa98
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2024-01-23 12:41:58 +0100

    Give read access on motebehov to DVH except two columns (#292)

--- src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
+++ src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
@@ -1,3 +1,7 @@
 REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
+REVOKE ALL ON ALL TABLES IN SCHEMA public FROM "disykefravar-x4wt@knada-gcp.iam";
+REVOKE ALL ON ALL TABLES IN SCHEMA public FROM "disykefravar-x4wt@knada-gcp.iam.gserviceaccount.com";
 
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
+
+GRANT SELECT ON MOTEBEHOV_DVH TO "disykefravar-x4wt@knada-gcp.iam.gserviceaccount.com";
