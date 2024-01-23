314d29863a6b1d4129e383935f030f8d335253cf
geir-waagboe <geir.arne.waagbo@nav.no>
2024-01-23 12:30:29 +0100

    Give read access on motebehov view to DVH

--- src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
+++ src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
@@ -4,19 +4,4 @@ REVOKE ALL ON ALL TABLES IN SCHEMA public FROM "disykefravar-x4wt@knada-gcp.iam.
 
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
 
-GRANT SELECT(
-    id,
-    motebehov_uuid,
-    opprettet_dato,
-    opprettet_av,
-    virksomhetsnummer,
-    har_motebehov,
-    tildelt_enhet,
-    behandlet_tidspunkt,
-    behandlet_veileder_ident,
-    skjematype,
-    sm_fnr,
-    opprettet_av_fnr)
-ON motebehov to "disykefravar-x4wt@knada-gcp.iam.gserviceaccount.com";
-
 GRANT SELECT ON MOTEBEHOV_DVH TO "disykefravar-x4wt@knada-gcp.iam.gserviceaccount.com";
