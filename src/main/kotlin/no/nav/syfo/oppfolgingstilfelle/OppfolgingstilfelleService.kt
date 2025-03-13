3945e989dbc580ded3011d325d7df52d49a7a5fc
malazkoj <malaz.alkoj@nav.no>
2025-03-13 13:28:18 +0100

    More logs for finding out why dialogmotefrontend gets 409.

--- src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/OppfolgingstilfelleService.kt
+++ src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/OppfolgingstilfelleService.kt
@@ -4,6 +4,7 @@ import no.nav.syfo.metric.Metric
 import no.nav.syfo.oppfolgingstilfelle.database.*
 import no.nav.syfo.oppfolgingstilfelle.kafka.domain.KafkaOppfolgingstilfellePerson
 import no.nav.syfo.oppfolgingstilfelle.kafka.domain.previouslyProcessed
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import javax.inject.Inject
 
@@ -82,13 +83,19 @@ class OppfolgingstilfelleService @Inject constructor(
         arbeidstakerFnr: String,
     ): List<PPersonOppfolgingstilfelle> {
         val oppfolgingstilfelleList = oppfolgingstilfelleDAO.get(arbeidstakerFnr)
-
+        log.info("Get oppfolgingstilfelleList, size: ${oppfolgingstilfelleList.size}")
         val activeOppfolgingstilfelleList = oppfolgingstilfelleList.filter {
             it.isSykmeldtLast16Days()
         }
+        log.info("""After filter isSykmeldtLast16days activeOppfolgingstilfelleList,
+            | size: ${activeOppfolgingstilfelleList.size}""".trimMargin())
         val expiredOppfolgingstilfelleList = oppfolgingstilfelleList.filterNot {
             it.isSykmeldtLast16Days()
         }
+
+        log.info("""After filterNOT isSykmeldtLast16days activeOppfolgingstilfelleList,
+            | size: ${activeOppfolgingstilfelleList.size}""".trimMargin())
+
         return when {
             activeOppfolgingstilfelleList.isEmpty() -> {
                 emptyList()
@@ -114,7 +121,7 @@ class OppfolgingstilfelleService @Inject constructor(
         val activeOppfolgingstilfeller: List<PersonOppfolgingstilfelle> = oppfolgingstilfelleList.map {
             it.mapToPersonOppfolgingstilfelle()
         }
-
+        log.info("Active oppfolgingstilfeller size: ${activeOppfolgingstilfeller.size}")
         return if (activeOppfolgingstilfeller.isNotEmpty()) {
             if (activeOppfolgingstilfeller.size > 1) {
                 val minFom = activeOppfolgingstilfeller.minByOrNull { it.fom }!!.fom
@@ -133,6 +140,7 @@ class OppfolgingstilfelleService @Inject constructor(
     }
 
     companion object {
+        private val log = LoggerFactory.getLogger(OppfolgingstilfelleService::class.java)
         private const val METRIC_RECEIVE_OPPFOLGINGSTILFELLE_BASE = "receive_oppfolgingstilfelle"
         private const val METRIC_RECEIVE_OPPFOLGINGSTILFELLE_CREATE = "${METRIC_RECEIVE_OPPFOLGINGSTILFELLE_BASE}_create"
         private const val METRIC_RECEIVE_OPPFOLGINGSTILFELLE_UPDATE = "${METRIC_RECEIVE_OPPFOLGINGSTILFELLE_BASE}_update"
