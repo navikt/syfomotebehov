8ddbc340e1167191b43064d74c2d08f884b7a300
Håkon Salomonsen Møller <hakon.salomonsen.moller@nav.no>
2025-04-01 10:27:18 +0200

    Uncomment Flyway script for new table

--- src/main/resources/db/migration/V1_11__motebehovFormValues.sql
+++ src/main/resources/db/migration/V1_11__motebehovFormValues.sql
@@ -1,24 +1,22 @@
--- -- Create the motebehovFormValues table
--- CREATE TABLE MOTEBEHOV_FORM_VALUES
--- (
---     motebehov_uuid                      VARCHAR(36) NOT NULL,
---     form_snapshot                       JSONB       NOT NULL,
---     -- The fields below are only used for debugging and data monitoring purposes
---     form_identifier                     VARCHAR(50) NOT NULL,
---     form_semantic_version               VARCHAR(20) NOT NULL,
---     begrunnelse                         TEXT,
---     onsker_sykmelder_deltar             BOOLEAN     NOT NULL,
---     onsker_sykmelder_deltar_begrunnelse TEXT,
---     onsker_tolk                         BOOLEAN     NOT NULL,
---     tolk_sprak                          TEXT
--- );
---
--- -- Create an index on the id field
--- CREATE INDEX idx_motebehov_form_values_motebehov_uuid ON MOTEBEHOV_FORM_VALUES (motebehov_uuid);
---
--- -- Add the new foreign key constraint from motebehov_form_values to motebehov with ON DELETE CASCADE
--- ALTER TABLE MOTEBEHOV_FORM_VALUES
---     ADD CONSTRAINT fk_motebehov
---         FOREIGN KEY (motebehov_uuid)
---             REFERENCES MOTEBEHOV (motebehov_uuid)
---             ON DELETE CASCADE;
+-- Create the motebehovFormValues table
+CREATE TABLE MOTEBEHOV_FORM_VALUES
+(
+    motebehov_uuid                      VARCHAR(36) NOT NULL,
+    form_snapshot                       JSONB       NOT NULL,
+    -- The fields below are only used for debugging and data monitoring purposes
+    form_identifier                     VARCHAR(50) NOT NULL,
+    form_semantic_version               VARCHAR(20) NOT NULL,
+    begrunnelse                         TEXT,
+    onsker_sykmelder_deltar             BOOLEAN     NOT NULL,
+    onsker_sykmelder_deltar_begrunnelse TEXT,
+    onsker_tolk                         BOOLEAN     NOT NULL,
+    tolk_sprak                          TEXT
+);
+
+CREATE INDEX idx_motebehov_form_values_motebehov_uuid ON MOTEBEHOV_FORM_VALUES (motebehov_uuid);
+
+ALTER TABLE MOTEBEHOV_FORM_VALUES
+    ADD CONSTRAINT fk_motebehov
+        FOREIGN KEY (motebehov_uuid)
+            REFERENCES MOTEBEHOV (motebehov_uuid)
+            ON DELETE CASCADE;
