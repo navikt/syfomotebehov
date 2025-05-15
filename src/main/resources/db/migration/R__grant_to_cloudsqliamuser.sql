e73553c3ed017df5cf548cb02c83b8f09f18e983
malazkoj <malaz.alkoj@nav.no>
2025-05-15 12:13:50 +0200

    Add grant esyfo-analyse to repeatable file.

--- src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
+++ src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
@@ -5,3 +5,5 @@ REVOKE ALL ON ALL TABLES IN SCHEMA public FROM "disykefravar-x4wt@knada-gcp.iam.
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
 
 GRANT SELECT ON MOTEBEHOV_DVH TO "disykefravar-x4wt@knada-gcp.iam.gserviceaccount.com";
+
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO "esyfo-analyse";
