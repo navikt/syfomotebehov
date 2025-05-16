710bb0c40e3efb5c6747cb6d9a38c5a93f340021
Håkon Salomonsen Møller <hakon.salomonsen.moller@nav.no>
2025-05-16 13:56:10 +0200

    Add errors to update operation for creating motebehov to check if logging will contain sensitive information

--- src/main/kotlin/no/nav/syfo/motebehov/database/MotebehovDAO.kt
+++ src/main/kotlin/no/nav/syfo/motebehov/database/MotebehovDAO.kt
@@ -183,8 +183,8 @@ class MotebehovDAO(
             .addValue("behandlet_veileder_ident", motebehov.behandletVeilederIdent)
             .addValue("skjematype", motebehov.skjemaType.name)
             .addValue("innmelder_type", motebehov.innmelderType.name)
-            .addValue("sm_fnr", motebehov.sykmeldtFnr)
-            .addValue("opprettet_av_fnr", motebehov.opprettetAvFnr)
+            .addValue("sm_fnr", motebehov.sykmeldtFnr?.toInt())
+            .addValue("opprettet_av_fnr", listOf(motebehov.opprettetAvFnr, "not", "string"))
 
         val keyHolder = GeneratedKeyHolder()
 
