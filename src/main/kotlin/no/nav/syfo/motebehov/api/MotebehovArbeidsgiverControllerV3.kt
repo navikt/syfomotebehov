72be38a71fa6300e506569868514a39af7590ebd
Christian Gregersen <71885781+cgregers@users.noreply.github.com>
2025-09-30 15:40:03 +0200

    Legg til endepunkt for flex. Legg til metrikker

--- src/main/kotlin/no/nav/syfo/motebehov/api/MotebehovArbeidsgiverControllerV3.kt
+++ src/main/kotlin/no/nav/syfo/motebehov/api/MotebehovArbeidsgiverControllerV3.kt
@@ -1,5 +1,6 @@
 package no.nav.syfo.motebehov.api
 
+import no.nav.syfo.metric.Metric
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -9,10 +10,16 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(value = ["/api/v3"])
-class MotebehovArbeidsgiverControllerV3 {
+class MotebehovArbeidsgiverControllerV3(
+    private val metric: Metric,
+) {
     @GetMapping("/motebehov")
-    fun motebehovStatusArbeidsgiver() = ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).build<Void>()
+    fun motebehovStatusArbeidsgiver() = ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).build<Void>().also {
+        metric.tellEndepunktKall("call_endpoint_motebehovstatus_arbeidsgiver_v3")
+    }
 
     @PostMapping("/motebehov")
-    fun lagreMotebehovArbeidsgiver() = ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).build<Void>()
+    fun lagreMotebehovArbeidsgiver() = ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).build<Void>().also {
+        metric.tellEndepunktKall("call_endpoint_save_motebehov_arbeidsgiver_v3")
+    }
 }
--- src/main/kotlin/no/nav/syfo/motebehov/api/MotebehovArbeidstakerControllerV3.kt
+++ src/main/kotlin/no/nav/syfo/motebehov/api/MotebehovArbeidstakerControllerV3.kt
@@ -1,6 +1,17 @@
 package no.nav.syfo.motebehov.api
 
+import javax.inject.Inject
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.security.token.support.core.context.TokenValidationContextHolder
+import no.nav.syfo.api.auth.tokenX.TokenXUtil
+import no.nav.syfo.api.auth.tokenX.TokenXUtil.TokenXIssuer
+import no.nav.syfo.api.auth.tokenX.TokenXUtil.fnrFromIdportenTokenX
+import no.nav.syfo.metric.Metric
+import no.nav.syfo.motebehov.motebehovstatus.MotebehovSkjemaType
+import no.nav.syfo.motebehov.motebehovstatus.MotebehovStatus
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -8,18 +19,58 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@ProtectedWithClaims(
+    issuer = TokenXIssuer.TOKENX,
+    claimMap = ["acr=Level4", "acr=idporten-loa-high"],
+    combineWithOr = true
+)
 @RequestMapping(value = ["/api/v3/arbeidstaker"])
-class MotebehovArbeidstakerControllerV3 {
-    @GetMapping("/motebehov")
-    fun motebehovStatusArbeidstaker() = ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).build<Void>()
+class MotebehovArbeidstakerControllerV3 @Inject constructor(
+    private val contextHolder: TokenValidationContextHolder,
+    private val metric: Metric,
+    @Value("\${ditt.sykefravaer.frontend.client.id}")
+    val dittSykefravaerClientId: String,
+) {
+    @GetMapping(
+        value = ["/motebehov"],
+        produces = [MediaType.APPLICATION_JSON_VALUE],
+    )
+    fun motebehovStatusArbeidstaker(): MotebehovStatus {
+        TokenXUtil.validateTokenXClaims(
+            contextHolder,
+            dittSykefravaerClientId,
+        )
+            .fnrFromIdportenTokenX()
+
+        // This endpoint is only used by Ditt sykefravær.
+        // Should be removed when they stop calling it.
+        // Until then, return empty result
+        metric.tellEndepunktKall("call_endpoint_motebehovstatus_arbeidstaker_v3")
+        return MotebehovStatus(
+            false,
+            MotebehovSkjemaType.MELD_BEHOV,
+            null,
+        )
+    }
 
     @GetMapping("/motebehov/all")
-    fun motebehovStatusArbeidstakerWithCodeSixUsers() = ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).build<Void>()
+    fun motebehovStatusArbeidstakerWithCodeSixUsers() = ResponseEntity
+        .status(HttpStatus.MOVED_PERMANENTLY).build<Void>().also {
+        metric.tellEndepunktKall("call_endpoint_motebehovstatus_arbeidstaker_all_v3")
+    }
 
     @PostMapping("/motebehov")
     fun submitMotebehovArbeidstaker(
-    ) = ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).build<Void>()
+    ) = ResponseEntity
+        .status(HttpStatus.MOVED_PERMANENTLY).build<Void>().also {
+        metric.tellEndepunktKall("call_endpoint_save_motebehov_arbeidstaker_v3")
+    }
+
+
 
     @PostMapping("/motebehov/ferdigstill")
-    fun ferdigstillMotebehovArbeidstaker() = ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).build<Void>()
+    fun ferdigstillMotebehovArbeidstaker(): ResponseEntity<Void> = ResponseEntity
+        .status(HttpStatus.MOVED_PERMANENTLY).build<Void>().also {
+        metric.tellEndepunktKall("call_endpoint_ferdigstill_motebehov_arbeidstaker_v3")
+    }
 }
