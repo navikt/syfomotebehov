52ad85758bfa5efa2a5f580c8cfa2f7c302d4e8a
malazkoj <malaz.alkoj@nav.no>
2026-02-03 09:54:45 +0100

    Delete for AG not for arbeidstaker.

--- src/main/resources/db/migration/V1_17__empty_wrong_formsnapshot_and_sm_fnr.sql
+++ src/main/resources/db/migration/V1_17__empty_wrong_formsnapshot_and_sm_fnr.sql
@@ -1,4 +1,4 @@
 DELETE FROM motebehov_form_values WHERE motebehov_row_id = '1357050';
 DELETE FROM motebehov WHERE id = '1357050';
-DELETE FROM motebehov_form_values WHERE motebehov_row_id = '1455825';
-DELETE FROM motebehov WHERE id = '1455825';
+DELETE FROM motebehov_form_values WHERE motebehov_row_id = '1456525';
+DELETE FROM motebehov WHERE id = '1456525';
