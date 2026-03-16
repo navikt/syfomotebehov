da565aebf0b128c9b66f131239c18e084b83647c
Christian Gregersen <71885781+cgregers@users.noreply.github.com>
2026-03-16 08:28:14 +0100

    delete redundant Bruno collection. Fix import IsOppfolgingstilfelleKafkaConfig

--- src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/kafka/IsOppfolgingstilfelleKafkaConfig.kt
+++ src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/kafka/IsOppfolgingstilfelleKafkaConfig.kt
@@ -2,6 +2,7 @@ package no.nav.syfo.oppfolgingstilfelle.kafka
 
 import no.nav.syfo.config.kafka.KafkaAivenConfig
 import no.nav.syfo.oppfolgingstilfelle.kafka.domain.KafkaOppfolgingstilfellePerson
+import no.nav.syfo.util.configuredJsonMapper
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.context.annotation.Bean
