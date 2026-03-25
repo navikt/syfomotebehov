5bbdaf003b5866f529bb79224e4aa833f4e9c8c8
Bjørn Faaberg <bjorn.faaberg@nav.no>
2026-03-25 10:33:07 +0100

    Downgrade kotlin to 2.3.10 for CodeQL

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,8 +29,8 @@ plugins {
     id("org.springframework.boot") version "4.0.3"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
-    kotlin("jvm") version "2.3.20"
-    kotlin("plugin.spring") version "2.3.20"
+    kotlin("jvm") version "2.3.10"
+    kotlin("plugin.spring") version "2.3.10"
 }
 
 repositories {
