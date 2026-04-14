de5bde9f24b6c67f43d59eca857b50ee89ff4989
André Wittwer <andre.wittwer@nav.no>
2026-04-14 15:28:07 +0200

    chore: revert kotlin version to 2.3.10 (#655)
    Revert Kotlin version to 2.3.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,8 +29,8 @@ plugins {
     id("org.springframework.boot") version "4.0.5"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
-    kotlin("jvm") version "2.3.20"
-    kotlin("plugin.spring") version "2.3.20"
+    kotlin("jvm") version "2.3.10"
+    kotlin("plugin.spring") version "2.3.10"
 }
 
 repositories {
