e3e2afd42441a8b79c770d5a8c55432e22a8ab18
Christian Gregersen <71885781+cgregers@users.noreply.github.com>
2026-06-23 12:35:16 +0200

    chore(permissions): modify R_ script to re-execute every deployment

--- src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
+++ src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
@@ -1,3 +1,4 @@
+-- ${flyway:timestamp} Inserts a timestamp to generate a new hash to trigger re-execution on deploy.
 REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
 REVOKE ALL ON ALL TABLES IN SCHEMA public FROM "disykefravar-x4wt@knada-gcp.iam";
 REVOKE ALL ON ALL TABLES IN SCHEMA public FROM "disykefravar-x4wt@knada-gcp.iam.gserviceaccount.com";
