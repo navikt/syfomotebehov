79e561e2da247d96df59de69a9336e4685f331b6
Christian Gregersen <71885781+cgregers@users.noreply.github.com>
2026-06-24 11:45:09 +0200

    chore(Flyway): move content from R_ script to afterMigrate lifecycle hook (#718)
    ## Sjekkliste
    
    - [x] Koden kompilerer og linter uten feil
    - [x] Endringene er testet (manuelt eller automatisk)
    - [x] Ingen sensitiv data eksponert (tokens, credentials, PII)

--- src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
+++ src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
@@ -1,10 +1,1 @@
--- ${flyway:timestamp} Inserts a timestamp to generate a new hash to trigger re-execution on deploy.
-REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
-REVOKE ALL ON ALL TABLES IN SCHEMA public FROM "disykefravar-x4wt@knada-gcp.iam";
-REVOKE ALL ON ALL TABLES IN SCHEMA public FROM "disykefravar-x4wt@knada-gcp.iam.gserviceaccount.com";
-
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
-
-GRANT SELECT ON MOTEBEHOV_DVH TO "disykefravar-x4wt@knada-gcp.iam.gserviceaccount.com";
-
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO "esyfo-analyse";
+-- moved to afterMigrate.sql
